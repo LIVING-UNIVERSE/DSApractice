eb53494553373d189040145ff35a7500b000a227
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2024-11-06 01:00:21 +0530

    Minimum platform

--- Greedy/Greddy.cpp
+++ Greedy/Greddy.cpp
@@ -19,14 +19,14 @@
 
 
 
-/*
+
 struct Job
 {
     int id;	 // Job Id
     int deadline; // Deadline of job
     int profit; // Profit if job is over before or on deadline
 };
-*/
+
 
 class Solution {
   public:
@@ -220,3 +220,95 @@ public:
         return true;
     }
 };
+
+/*
+Minimum Platforms
+Difficulty: MediumAccuracy: 26.84%Submissions: 477K+Points: 4
+Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train is kept waiting.
+Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.
+
+Note: Time intervals are in the 24-hour format(HHMM) , where the first two characters represent hour (between 00 to 23 ) and the last two characters represent minutes (this will be <= 59 and >= 0).
+
+Examples:
+
+Input: arr[] = [0900, 0940, 0950, 1100, 1500, 1800], 
+            dep[] = [0910, 1200, 1120, 1130, 1900, 2000]
+Output: 3
+Explanation: There are three trains during the time 0940 to 1200. So we need minimum 3 platforms.
+Input: arr[] = [0900, 1235, 1100], 
+            dep[] = [1000, 1240, 1200]
+Output: 1
+Explanation: All train times are mutually exlusive. So we need only one platform
+Input: arr[] = [1000, 0935, 1100], 
+            dep[] = [1200, 1240, 1130]
+Output: 3
+Explanation: All 3 trains have to be their from 11:00 to 11:30
+Expected Time Complexity: O(nLogn)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+
+1≤ number of trains ≤ 50000
+0000 ≤ arr[i] ≤ dep[i] ≤ 2359
+
+
+*/
+
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int ap=0,dp=0;
+        int plat=1,count=0;
+        while(ap<n && dp<n){
+            if(arr[ap]<=dep[dp]){
+                count++;
+                ap++;
+            }
+            else{
+                count--;
+                dp++;
+            }
+            plat=max(plat,count);
+        }
+        return plat;
+    }
+};
+
+
+//{ Driver Code Starts.
+//  Driver code
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        vector<int> dep(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        for (int j = 0; j < n; j++) {
+            cin >> dep[j];
+        }
+        Solution ob;
+        cout << ob.findPlatform(arr, dep) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
