13feb8ee250e41c0f1204f7d9e1cedb4d9bd5601
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2024-12-29 17:25:25 +0530

    house robber 1

--- DP/dp1.cpp
+++ DP/dp1.cpp
@@ -91,90 +91,311 @@
 
 
 
-// // Geek Jump
-// Difficulty: EasyAccuracy: 49.55%Submissions: 100K+Points: 2
-//  Geek wants to climb from the 0th stair to the (n-1)th stair. At a time the Geek can climb either one or two steps. A height[N] array is also given. Whenever the geek jumps from stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. return the minimum energy that can be used by the Geek to jump from stair 0 to stair N-1.
+// // // Geek Jump
+// // Difficulty: EasyAccuracy: 49.55%Submissions: 100K+Points: 2
+// //  Geek wants to climb from the 0th stair to the (n-1)th stair. At a time the Geek can climb either one or two steps. A height[N] array is also given. Whenever the geek jumps from stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. return the minimum energy that can be used by the Geek to jump from stair 0 to stair N-1.
 
-// Example:
+// // Example:
 
  
 
-// Input: n = 4, height = {10 20 30 10}
-// Output: 20
-// Explanation:
-// Geek jump from 1st to 2nd stair(|20-10| = 10 energy lost). Then a jump from the 2nd to the last stair(|10-20| = 10 energy lost). So, total energy lost is 20 which is the minimum.
-// Your Task:
-// You don't need to read input or print anything. Your task is to complete the function MinimumEnergy() which takes the array height, and integer n, and returns the minimum energy that is lost.
+// // Input: n = 4, height = {10 20 30 10}
+// // Output: 20
+// // Explanation:
+// // Geek jump from 1st to 2nd stair(|20-10| = 10 energy lost). Then a jump from the 2nd to the last stair(|10-20| = 10 energy lost). So, total energy lost is 20 which is the minimum.
+// // Your Task:
+// // You don't need to read input or print anything. Your task is to complete the function MinimumEnergy() which takes the array height, and integer n, and returns the minimum energy that is lost.
 
-// Expected Time Complexity: O(n)
-// Expected Space Complexity: O(n)
+// // Expected Time Complexity: O(n)
+// // Expected Space Complexity: O(n)
+
+// // Constraint:
+// // 1<=n<=100000
+// // 1<=height[i]<=1000
+
+
+// //{ Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// // } Driver Code Ends
+// class Solution {
+//   private:
+//     int helper(vector<int> &H,vector<int> &dp,int n){
+//         if(n==0) return 0;
+//         if(n==1) return abs(H[1]-H[0]);
+//         if(dp[n]!=-1) return dp[n];
+//         int l= helper(H,dp,n-1) + abs(H[n]-H[n-1]);
+//         int r= helper(H,dp,n-2) + abs(H[n]-H[n-2]);
+//         return dp[n]=min(l,r); 
+//     }
+//   public:
+//     int minimumEnergy(vector<int>& height, int n) {
+//         // top dowm (memoization)
+//         // vector<int> dp(n,-1);
+//         // return helper(height,dp,n-1);
+        
+//         // vector<int> dp(n,-1);
+//         // dp[0]=0;
+//         // dp[1]=abs(height[1]-height[0]);
+//         // for(int i=2; i<n; i++){
+//         //     int l= dp[i-1]+abs(height[i]-height[i-1]);
+//         //     int r= dp[i-2]+abs(height[i]-height[i-2]);
+//         //     dp[i]=min(l,r);
+//         // }
+//         // return dp[n-1];
+
+//         int prev2=0;
+//         int prev=abs(height[1]-height[0]);
+//         for(int i=2; i<n; i++){
+//             int l= prev+abs(height[i]-height[i-1]);
+//             int r= prev2+abs(height[i]-height[i-2]);
+//             prev2=prev;
+//             prev=min(l,r);
+//         }
+//         return prev;
+//     }
+// };
+
+// //{ Driver Code Starts.
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int N;
+//         cin >> N;
+//         vector<int> arr(N);
+//         for (int i = 0; i < N; i++) {
+//             cin >> arr[i];
+//         }
+//         Solution obj;
+//         cout << obj.minimumEnergy(arr, N) << "\n";
+    
+// cout << "~" << "\n";
+// }
+//     return 0;
+// }
+// // } Driver Code Ends
+
+
+
+
+
+// Minimal Cost
+// Difficulty: MediumAccuracy: 51.08%Submissions: 85K+Points: 4
+// There is an array arr of heights of stone and Geek is standing at the first stone and can jump to one of the following: Stone i+1, i+2, ... i+k stone, where k is the maximum number of steps that can be jumped and cost will be |hi-hj| is incurred, where j is the stone to land on. Find the minimum possible total cost incurred before the Geek reaches the last stone.
+
+// Example:
+
+// Input: k = 3, arr[]= [10, 30, 40, 50, 20]
+// Output: 30
+// Explanation: Geek will follow the path 1->2->5, the total cost would be | 10-30| + |30-20| = 30, which is minimum
+// Input: k = 1, arr[]= [10, 20, 10]
+// Output: 20
+// Explanation: Geek will follow the path 1->2->3, the total cost would be |10 - 20| + |20 - 10| = 20.
+// Expected Time Complexity: O(n*k)
+// Expected Auxilary Space: O(n)
 
 // Constraint:
-// 1<=n<=100000
-// 1<=height[i]<=1000
+// 1<= arr.size() <=104
+// 1 <= k <= 100
+// 1 <= arr[i] <= 104
 
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+
+// // top down 
+// //{ Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
 
 
-// } Driver Code Ends
-class Solution {
-  private:
-    int helper(vector<int> &H,vector<int> &dp,int n){
-        if(n==0) return 0;
-        if(n==1) return abs(H[1]-H[0]);
-        if(dp[n]!=-1) return dp[n];
-        int l= helper(H,dp,n-1) + abs(H[n]-H[n-1]);
-        int r= helper(H,dp,n-2) + abs(H[n]-H[n-2]);
-        return dp[n]=min(l,r); 
-    }
-  public:
-    int minimumEnergy(vector<int>& height, int n) {
-        // top dowm (memoization)
-        // vector<int> dp(n,-1);
-        // return helper(height,dp,n-1);
+// // } Driver Code Ends
+// class Solution {
+//   private:
+//     int helper(vector<int> &arr,vector<int> &dp, int k, int n){
+//         if(n==0) return 0;
+//         if(dp[n]!=-1) return dp[n];
+//         int res=INT_MAX;
+//         for(int i=1; i<=k ;i++){
+//             if(n-i>=0){
+//                 int cash= helper(arr,dp,k,n-i) + abs(arr[n]-arr[n-i]);
+//                 res=min(res,cash);
+//             }
+//         }
+//         return dp[n]=res;
+//     }
+//   public:
+//     int minimizeCost(int k, vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int> dp(n,-1);
+//         return helper(arr,dp,k,n-1);
+//     }
+// };
+
+// //{ Driver Code Starts.
+
+// int main() {
+//     string ts;
+//     getline(cin, ts);
+//     int t = stoi(ts);
+//     while (t--) {
+//         string ks;
+//         getline(cin, ks);
+//         int k = stoi(ks);
+//         vector<int> arr;
+//         string input;
+//         getline(cin, input);
+//         stringstream ss(input);
+//         int number;
+//         while (ss >> number) {
+//             arr.push_back(number);
+//         }
+//         Solution obj;
+//         int res = obj.minimizeCost(k, arr);
+//         cout << res << endl;
+//         cout << "~" << endl;
+//         // string tl;
+//         // getline(cin, tl);
+//     }
+//     return 0;
+// }
+
+// // } Driver Code Ends
+
+
+// // bottoms up appraoch
+//  public:
+//     int minimizeCost(int k, vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int> dp(n,-1);
+//         // return helper(arr,dp,k,n-1);
+//         // bottoms up appraoch
+//         dp[0]=0;
+//         for(int i=1;i<n; i++){
+//             int res=INT_MAX;
+//             for(int j=1; j<=k ;j++){
+//                 if(i-j>=0){
+//                     int cash= dp[i-j]+abs(arr[i]-arr[i-j]);
+//                     res=min(res,cash);
+//                 }
+//             }
+//             dp[i]=res;
+//         }
+//         return dp[n-1];
+//     }
+
+// space optimization
+// public:
+//     int minimizeCost(int k, vector<int>& arr) {
+//         int n = arr.size();
+//         vector<int> dp(k, INT_MAX); // Use a k-size array
+//         dp[0] = 0; // Base case: cost to reach the first stone is 0
+
+//         for (int i = 1; i < n; i++) {
+//             int curr = INT_MAX;
+
+//             for (int j = 1; j <= k; j++) {
+//                 if (i - j >= 0) {
+//                     curr = min(curr, dp[(i - j) % k] + abs(arr[i] - arr[i - j]));
+//                 }
+//             }
+
+//             dp[i % k] = curr; // Update the sliding window
+//         }
+
+//         return dp[(n - 1) % k]; // The result is in the index corresponding to the last stone
+//     }
+// };
+
+
+
+
+
+
+
+// 198. House Robber
+// Solved
+// Medium
+// Topics
+// Companies
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+// Example 2:
+
+// Input: nums = [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+
+
+
+// top down
+// class Solution {
+// private:
+// // topDown approach
+//     int f(vector<int> &arr,vector<int> &dp,int ind){
+//         if(ind==0) return arr[ind];
+//         if(ind ==1) return max(arr[1],arr[0]);
+//         if(dp[ind]!=-1) return dp[ind];
+//         int pick= arr[ind]+ f(arr,dp,ind-2);
+//         int dpick = f(arr,dp,ind-1);
+//         return dp[ind]=max(pick,dpick);
+//     }
+// public:
+//     int rob(vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int> dp(n,-1);
+//         return f(arr,dp,n-1);
+//     }
+// };
+
+
+// // 2nd top down approach
+// int f(vector<int> &arr,vector<int> &dp,int ind){
+//     if(ind==0) return arr[0];
+//     if(ind<0) return 0;
+//     if(dp[ind]!=-1) return dp[ind];
+//     int pick= f(arr,dp,ind-2) +arr[ind];
+//     int dpick= f(arr,dp,ind-1);
+//     return dp[ind]= max(pick,dpick);
+// }
+
+
+
+// bottom up
+// public:
+//     int rob(vector<int>& arr) {
+//         int n=arr.size();
+//         int prev2= 0;
         
-        // vector<int> dp(n,-1);
-        // dp[0]=0;
-        // dp[1]=abs(height[1]-height[0]);
-        // for(int i=2; i<n; i++){
-        //     int l= dp[i-1]+abs(height[i]-height[i-1]);
-        //     int r= dp[i-2]+abs(height[i]-height[i-2]);
-        //     dp[i]=min(l,r);
-        // }
-        // return dp[n-1];
-
-        int prev2=0;
-        int prev=abs(height[1]-height[0]);
-        for(int i=2; i<n; i++){
-            int l= prev+abs(height[i]-height[i-1]);
-            int r= prev2+abs(height[i]-height[i-2]);
-            prev2=prev;
-            prev=min(l,r);
-        }
-        return prev;
-    }
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<int> arr(N);
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
-        Solution obj;
-        cout << obj.minimumEnergy(arr, N) << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
+//         int prev= arr[0];
+//         for(int i=1; i<n; i++){
+//             int take = arr[i];
+//             if(i>1){
+//                 take+=prev2;
+//             }
+//             int dtake = 0+prev;
+//             int res=max(take,dtake);
+//             prev2=prev;
+//             prev=res;
+//         }
+//         return prev;
+//     }
