3bbce2aa597cd487eb4a04b25c60bcdaf529c552
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-03 15:43:06 +0530

    unique path I and II

--- DP/dp1.cpp
+++ DP/dp1.cpp
@@ -508,4 +508,204 @@
 
 
 
+// //62. Unique Paths
+// Solved
+// Medium
+// Topics
+// Companies
+// Google
+// Facebook
+// Microsoft
+// Amazon
+// Apple
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+// Example 1:
+
+
+// Input: m = 3, n = 7
+// Output: 28
+// Example 2:
+
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+ 
+
+// Constraints:
+
+// 1 <= m, n <= 100
+
+
+
+// // top down approach
+// class Solution {
+// private:
+//     int f(int x, int y, vector<vector<int>> &dp){
+//         if(x==0 || y==0) return 1;
+//         if(dp[x][y]!=-1) return dp[x][y];
+//         int up=0,left=0;
+//         left= f(x-1,y,dp);
+//         up=f(x,y-1,dp);
+//         return dp[x][y]=left+up;
+//     }
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return f(m-1,n-1,dp);
+//     }
+// };
+
+
+
+// // bottoms up approach
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         for(int i=0; i<n; i++){
+//             dp[0][i]=1;
+//         }
+//         for(int i=1; i<m; i++){
+//             dp[i][0]=1;
+//         }
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++){
+//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+
+
+// // bottoms up (space optimization)
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<int> dp(n,1);
+//         for(int i=1; i<m; i++){
+//             vector<int> temp(n,1);
+//             for(int j=1; j<n; j++){
+//                 temp[j]=temp[j-1]+dp[j];
+//             }
+//             dp=temp;
+//         }
+//         return dp[n-1];
+//     }
+// };
+
+
+
+
+
+
+
+
+// //63. Unique Paths II
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Cruise Automation
+// Facebook
+// Amazon
+// Qualtrics
+// Google
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+// Example 1:
+
+
+// Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: 2
+// Explanation: There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+// Example 2:
+
+
+// Input: obstacleGrid = [[0,1],[0,0]]
+// Output: 1
+ 
+
+// Constraints:
+
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] is 0 or 1.
+
+
+// // tup down approach
+// class Solution {
+// private:
+//     int f(int row, int col, vector<vector<int>>& dp,
+//           vector<vector<int>>& grid) {
+//         if (grid[row][col] == 1) return 0;
+//         if (row == 0 && col == 0) return 1;
+//         if (dp[row][col]!=-1) return dp[row][col];
+//         int left = 0, up = 0;
+//         if (col - 1 >= 0 ) {
+//             left = f(row, col - 1, dp, grid);
+//         }
+//         if (row - 1 >= 0 ) {
+//             up = f(row - 1, col, dp, grid);
+//         }
+//         return dp[row][col]=left + up;
+//     }
+
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         if (grid[m - 1][n - 1] == 1 || grid[0][0] == 1) {
+//             return 0;
+//         }
+//         return f(m - 1, n - 1, dp, grid);
+//     }
+// };
+
+
+// // bottoms up approach
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+//         for (int row = 0; row < m; row++) {
+//             for (int col = 0; col < n; col++) {
+//                 if(grid[row][col]==1) dp[row][col]=0;
+//                 else if(row==0 && col==0) dp[row][col]=1;
+//                 else{
+//                     int left=0,up=0;
+//                     if(row>0) up=dp[row-1][col];
+//                     if(col>0) left=dp[row][col-1];
+//                     dp[row][col]= up+left;
+//                 }
+//             }
+//         }
+//         return dp[m - 1][n - 1];
+//     }
+// };
