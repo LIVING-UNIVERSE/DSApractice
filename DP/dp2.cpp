52310fdb67cd8d0e837870c15a482681fb6bc721
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-09 16:23:59 +0530

    Partitions with Given Difference

--- DP/dp2.cpp
+++ DP/dp2.cpp
@@ -549,3 +549,146 @@ class Solution {
         return prev[target];
     }
 };
+
+
+
+//Partitions with Given Difference
+Difficulty: MediumAccuracy: 36.76%Submissions: 156K+Points: 4
+Given an array arr[], partition it into two subsets(possibly empty) such that each element must belong to only one subset. Let the sum of the elements of these two subsets be sum1 and sum2. Given a difference d, count the number of partitions in which sum1 is greater than or equal to sum2 and the difference between sum1 and sum2 is equal to d. 
+
+Examples :
+
+Input: arr[] =  [5, 2, 6, 4], d = 3
+Output: 1
+Explanation: There is only one possible partition of this array. Partition : {6, 4}, {5, 2}. The subset difference between subset sum is: (6 + 4) - (5 + 2) = 3.
+Input: arr[] = [1, 1, 1, 1], d = 0 
+Output: 6 
+Explanation: We can choose two 1's from indices {0,1}, {0,2}, {0,3}, {1,2}, {1,3}, {2,3} and put them in sum1 and remaning two 1's in sum2.
+Thus there are total 6 ways for partition the array arr. 
+Input: arr[] = [1, 2, 1, 0, 1, 3, 3], d = 11
+Output: 2
+Constraint:
+1 <= arr.size() <= 50
+0 <= d  <= 50
+0 <= arr[i] <= 6
+
+
+
+//top down approach
+class Solution {
+  private:
+    int f(int ind,int target, vector<int> &arr,vector<vector<int>> &dp){
+        if(ind==0){
+            if(target==0 && arr[0]==0) return 2;
+            if(target==0) return 1;
+            if(target==arr[0]) return 1;
+            return 0;
+        }
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int notTaken = f(ind-1,target,arr,dp);
+        int taken=0;
+        if(target>=arr[ind]){
+            taken=f(ind-1,target-arr[ind],arr,dp);
+        }
+        return dp[ind][target]=taken+notTaken;
+    }
+  public:
+    int countPartitions(vector<int>& arr, int d) {
+        // s1-s2=d  ;(s2=t-s1)
+        // s1-t+s1=d
+        // 2s1=t+d  =>   s1=(t+d)/2
+        // => target=(t+d)/2
+        int n=arr.size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+        if((sum+d)%2!=0) return 0;
+        int target=(sum+d)/2;
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        return f(n-1,target,arr,dp);
+    }
+};
+
+
+
+// bottoms up approach
+class Solution {
+
+  public:
+    int countPartitions(vector<int>& arr, int d) {
+        // s1-s2=d  ;(s2=t-s1)
+        // s1-t+s1=d
+        // 2s1=t+d  =>   s1=(t+d)/2
+        // => target=(t+d)/2
+        int n=arr.size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+        if((sum+d)%2!=0) return 0;
+        int target=(sum+d)/2;
+        vector<vector<int>> dp(n,vector<int>(target+1,0));
+        if(arr[0]<=target){
+            dp[0][arr[0]]=1;
+        }
+        if(arr[0]==0){
+            dp[0][0]=2;
+        }
+        else{
+            dp[0][0]=1;
+        }
+        for(int i=1; i<n; i++){
+            for(int t=0; t<=target; t++){
+                int notTaken=dp[i-1][t];
+                int taken=0;
+                if(t>=arr[i]){
+                    taken=dp[i-1][t-arr[i]];
+                }
+                dp[i][t]=notTaken+taken;
+            }
+        }
+        return dp[n-1][target];
+    }
+};
+
+
+// bottoms up approach(space optimized)
+class Solution {
+
+  public:
+    int countPartitions(vector<int>& arr, int d) {
+        // s1-s2=d  ;(s2=t-s1)
+        // s1-t+s1=d
+        // 2s1=t+d  =>   s1=(t+d)/2
+        // => target=(t+d)/2
+        int n=arr.size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+        if((sum+d)%2!=0) return 0;
+        int target=(sum+d)/2;
+        vector<vector<int>> dp(n,vector<int>(target+1,0));
+        if(arr[0]<=target){
+            dp[0][arr[0]]=1;
+        }
+        if(arr[0]==0){
+            dp[0][0]=2;
+        }
+        else{
+            dp[0][0]=1;
+        }
+        for(int i=1; i<n; i++){
+            for(int t=0; t<=target; t++){
+                int notTaken=dp[i-1][t];
+                int taken=0;
+                if(t>=arr[i]){
+                    taken=dp[i-1][t-arr[i]];
+                }
+                dp[i][t]=notTaken+taken;
+            }
+        }
+        return dp[n-1][target];
+    }
+};
