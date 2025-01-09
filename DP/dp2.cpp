3cf6502fb87948cf64455d7c0193900ef5fc9c2b
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-09 17:22:40 +0530

    Target Sum

--- DP/dp2.cpp
+++ DP/dp2.cpp
@@ -692,3 +692,90 @@ class Solution {
         return dp[n-1][target];
     }
 };
+
+
+
+
+//494. Target Sum
+Solved
+Medium
+Topics
+Companies
+Facebook
+Amazon
+Google
+Adobe
+Bloomberg
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+Example 2:
+
+Input: nums = [1], target = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+0 <= sum(nums[i]) <= 1000
+-1000 <= target <= 1000
+
+
+
+
+//(same as partition with given difference but here targets can be -ve so take absolute of target)
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& arr, int d) {
+        int n=arr.size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+        d=abs(d);
+        if((sum+d)%2!=0) return 0;
+        int target=(sum+d)/2;
+        // vector<vector<int>> dp(n,vector<int>(target+1,0));
+        vector<int> prev(target+1,0);
+        if(arr[0]<=target){
+            prev[arr[0]]=1;
+        }
+        if(arr[0]==0){
+            prev[0]=2;
+        }
+        else{
+            prev[0]=1;
+        }
+        for(int i=1; i<n; i++){
+            vector<int> curr(target+1,0);
+            for(int t=0; t<=target; t++){
+                int notTaken=prev[t];
+                int taken=0;
+                if(t>=arr[i]){
+                    taken=prev[t-arr[i]];
+                }
+                curr[t]=notTaken+taken;
+            }
+            prev=curr;
+        }
+        return prev[target];
+    }
+};
