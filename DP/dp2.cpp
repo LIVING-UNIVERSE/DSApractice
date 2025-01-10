96f1f4d5d028d88de64639b8a2f07ea3b7a0d353
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-10 16:22:01 +0530

    Rod Cutting

--- DP/dp2.cpp
+++ DP/dp2.cpp
@@ -1167,3 +1167,79 @@ class Solution {
         return prev[cap];
     }
 };
+
+
+
+
+// // Rod Cutting
+// Difficulty: MediumAccuracy: 60.66%Submissions: 152K+Points: 4
+// Given a rod of length n(size of price) inches and an array of prices, price. price[i] denotes the value of a piece of length i. Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+
+// Example:
+
+// Input: price[] = [1, 5, 8, 9, 10, 17, 17, 20]
+// Output: 22
+// Explanation: The maximum obtainable value is 22 by cutting in two pieces of lengths 2 and 6, i.e., 5+17=22.
+// Input: price[] = [3, 5, 8, 9, 10, 17, 17, 20]
+// Output: 24
+// Explanation: The maximum obtainable value is 24 by cutting the rod into 8 pieces of length 1, i.e, 8*price[1]= 8*3 = 24.
+// Input: price[] = [1, 10, 3, 1, 3, 1, 5, 9]
+// Output: 40
+// Constraints:
+// 1 ≤ price.size() ≤ 103
+// 1 ≤ price[i] ≤ 106
+
+
+
+// bottoms up approach(same as unbounded knapsack problem)
+class Solution {
+  public:
+    int cutRod(vector<int> &price) {
+        int n=price.size();
+        vector<vector<int>> dp(n,vector<int>(n+1,0));
+        for(int i=0; i<=n; i++){
+            if(i>=1){
+                dp[0][i]=i*price[0];
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=n; j++){
+                int notTaken=dp[i-1][j];
+                int taken=-1e8;
+                if(j>=i+1){
+                    taken=price[i]+dp[i][j-i-1];
+                }
+                dp[i][j]=max(taken,notTaken);
+            }
+        }
+        return dp[n-1][n];
+    }
+};
+
+
+// bottoms up (1D space optimization)
+class Solution {
+  public:
+    int cutRod(vector<int> &price) {
+        int n=price.size();
+        // vector<vector<int>> dp(n,vector<int>(n+1,0));
+        vector<int> prev(n+1,0);
+        for(int i=0; i<=n; i++){
+            if(i>=1){
+                prev[i]=i*price[0];
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=n; j++){
+                int notTaken=prev[j];
+                int taken=-1e8;
+                if(j>=i+1){
+                    taken=price[i]+prev[j-i-1];
+                }
+                prev[j]=max(taken,notTaken);
+            }
+        }
+        return prev[n];
+    }
+};
+
