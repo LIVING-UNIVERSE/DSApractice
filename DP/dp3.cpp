9d4231a23ac5290b9276eaffef216d8aa378b4c5
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-14 04:24:28 +0530

    Longest Common Substring

--- DP/dp3.cpp
+++ DP/dp3.cpp
@@ -195,3 +195,47 @@ class Solution {
 
 
 
+
+//Longest Common Substring
+Difficulty: MediumAccuracy: 42.69%Submissions: 260K+Points: 4
+You are given two strings s1 and s2. Your task is to find the length of the longest common substring among the given strings.
+
+Examples:
+
+Input: s1 = "ABCDGH", s2 = "ACDGHR"
+Output: 4
+Explanation: The longest common substring is "CDGH" with a length of 4.
+Input: s1 = "abc", s2 = "acb"
+Output: 1
+Explanation: The longest common substrings are "a", "b", "c" all having length 1.
+Input: s1 = "YZ", s2 = "yz"
+Output: 0
+Constraints:
+1 <= s1.size(), s2.size() <= 103
+Both strings may contain upper and lower case alphabets.
+
+
+
+
+// tabular solution
+(some problems are can only be efficiently solved by tabular method. Here if s1[ind1]==s2[ind2] we add 1 to previous call and 
+if they dont match just put zero and not always dp[n-1][m-1] will give you answer; just like here we had to take maximum of 
+all elements in the table)
+
+class Solution {
+  public:
+    int longestCommonSubstr(string& s1, string& s2) {
+        int n1=s1.size(),n2=s2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        int res=0;
+        for(int i=1;i<=n1; i++){
+            for(int j=1; j<=n2;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    res=max(res,dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+};
