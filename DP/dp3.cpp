3900bd9a6c2bffe5b3a71433e562ee87fa471b76
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-14 18:39:57 +0530

    Minimum Insertion Steps to Make a String Palindrome

--- DP/dp3.cpp
+++ DP/dp3.cpp
@@ -369,3 +369,95 @@ public:
         return prev[n-1];
     }
 };
+
+
+
+
+
+//1312. Minimum Insertion Steps to Make a String Palindrome
+Solved
+1787
+Hard
+Topics
+Companies
+Hint
+Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+ 
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+
+
+
+NOTE-> MOST OF THE DP ON STRING QUESTIONS CAN BE SOLVED BY USING LCS(LONGEST COMMON SUBSEQUENCE) .
+// bottoms up approach
+    class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=n; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
+
+
+
+// bottoms up approach(space optimized);
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.size();
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=n; j++){
+                if(s[i-1]==t[j-1]){
+                    curr[j]=1+prev[j-1];
+                }
+                else{
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            prev=curr;
+        }
+        return n-prev[n];
+    }
+};
