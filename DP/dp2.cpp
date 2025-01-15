b22b44d88ec6816f6bd770d6832d8dc3f38116a1
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-16 00:54:25 +0530

    Best Time to Buy and Sell Stock

--- DP/dp2.cpp
+++ DP/dp2.cpp
@@ -231,6 +231,88 @@ public:
 
 
 
+//2035. Partition Array Into Two Arrays to Minimize Sum Difference
+Attempted
+Hard
+Topics
+Companies
+Hint
+You are given an integer array nums of 2 * n integers. You need to partition nums into two arrays of length n to minimize the absolute difference of the sums of the arrays. To partition nums, put each element of nums into one of the two arrays.
+
+Return the minimum possible absolute difference.
+
+ 
+
+Example 1:
+
+example-1
+Input: nums = [3,9,7,3]
+Output: 2
+Explanation: One optimal partition is: [3,9] and [7,3].
+The absolute difference between the sums of the arrays is abs((3 + 9) - (7 + 3)) = 2.
+Example 2:
+
+Input: nums = [-36,36]
+Output: 72
+Explanation: One optimal partition is: [-36] and [36].
+The absolute difference between the sums of the arrays is abs((-36) - (36)) = 72.
+Example 3:
+
+example-3
+Input: nums = [2,-1,0,4,-2,-9]
+Output: 0
+Explanation: One optimal partition is: [2,4,-9] and [-1,0,-2].
+The absolute difference between the sums of the arrays is abs((2 + 4 + -9) - (-1 + 0 + -2)) = 0.
+ 
+
+Constraints:
+
+1 <= n <= 15
+nums.length == 2 * n
+-107 <= nums[i] <= 107
+
+
+
+// basically here the constraints are -10^7  to 10^7 so dp solution will not work
+// dp solution
+class Solution {
+public:
+    int minimumDifference(vector<int>& arr) {
+        int n=arr.size();
+        int sum=accumulate(arr.begin(),arr.end(),0);
+        vector<bool> prev(sum+1,false),curr(sum+1,false);
+        prev[0]=true;
+        if(arr[0]<=sum){
+            prev[arr[0]]=true;
+        }
+        for(int i=1; i<n; i++){
+            curr[0]=true;
+            for(int j=1;j<=sum;j++){
+                bool notTaken=prev[j];
+                bool taken=false;
+                if(arr[i]<=j){
+                    taken=prev[j-arr[i]];
+                }
+                curr[j]=taken||notTaken;
+            }
+            prev=curr;
+        }
+        int res=1e8;
+        for(int i=0; i<=sum/2; i++){
+            if(prev[i]){
+                int diff=abs(sum-2*i);
+                res=min(res,diff);
+            }
+        }
+        return res;
+    }
+};
+
+
+
+// 
+
+
 
 //213. House Robber II
 Solved
