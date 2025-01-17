9105d0e43bf44059cb3a8b7d292bd68278d64fb5
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-17 23:15:30 +0530

    Best Time to Buy and Sell Stock III

--- DP/dp4(dpOnStocks).cpp
+++ DP/dp4(dpOnStocks).cpp
@@ -400,3 +400,124 @@ public:
     }
 };
 
+
+
+
+//123. Best Time to Buy and Sell Stock III
+Solved
+Hard
+Topics
+Companies
+Amazon
+Uber
+Apple
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 105
+
+
+
+// top down approach
+class Solution {
+private:
+    int f(int ind,int buy,vector<int> &arr,vector<vector<int>> &dp){
+        if(buy==0 || ind==arr.size()) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        if(buy%2==0){
+            int notBought=f(ind+1,buy,arr,dp);
+            int bought=-arr[ind]+f(ind+1,buy-1,arr,dp);
+            return dp[ind][buy]= max(bought,notBought);
+        }
+        int notSell=f(ind+1,buy,arr,dp);
+        int sell=arr[ind]+f(ind+1,buy-1,arr,dp);
+        return dp[ind][buy]= max(sell,notSell);
+    }
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(5,-1));
+        return f(0,4,arr,dp);
+    }
+};
+
+
+
+// bottoms up approach
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(5,0));
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=4; j++){
+                if(j%2==0){
+                    int notBought=dp[i+1][j];
+                    int bought=-arr[i]+dp[i+1][j-1];
+                    dp[i][j]= max(bought,notBought);
+                }
+                else{
+                    int notSell=dp[i+1][j];
+                    int sell=arr[i]+dp[i+1][j-1];
+                    dp[i][j]= max(sell,notSell); 
+                }
+            }
+        }
+        return dp[0][4];
+    }
+};
+
+
+
+// bottoms up approach(space optimized)
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> prev(5,0),curr(5,0);
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=4; j++){
+                if(j%2==0){
+                    int notBought=prev[j];
+                    int bought=-arr[i]+prev[j-1];
+                    curr[j]= max(bought,notBought);
+                }
+                else{
+                    int notSell=prev[j];
+                    int sell=arr[i]+prev[j-1];
+                    curr[j]= max(sell,notSell); 
+                }
+            }
+            prev=curr;
+        }
+        return prev[4];
+    }
+};
+
