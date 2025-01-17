9c2a98cd5a305533eba13908b3fc1fea6befebd1
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-17 23:18:18 +0530

    Best Time to Buy and Sell Stock IV

--- DP/dp4(dpOnStocks).cpp
+++ DP/dp4(dpOnStocks).cpp
@@ -521,3 +521,71 @@ public:
     }
 };
 
+
+
+//188. Best Time to Buy and Sell Stock IV
+Solved
+Hard
+Topics
+Companies
+Amazon
+Apple
+Nvidia
+Google
+Citadel
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+
+Find the maximum profit you can achieve. You may complete at most k transactions: i.e. you may buy at most k times and sell at most k times.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: k = 2, prices = [2,4,1]
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Example 2:
+
+Input: k = 2, prices = [3,2,6,5,0,3]
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ 
+
+Constraints:
+
+1 <= k <= 100
+1 <= prices.length <= 1000
+0 <= prices[i] <= 1000
+
+
+
+// this is just same as the previous problem Best Time to Buy and Sell Stock III ; here you have to do 'k' transactions instead
+//of 2 so replace 4 by 2*k
+
+
+// bottoms up approach(space optimized)
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& arr) {
+         int n=arr.size();
+        vector<int> prev(2*k+1,0),curr(2*k+1,0);
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=2*k; j++){
+                if(j%2==0){
+                    int notBought=prev[j];
+                    int bought=-arr[i]+prev[j-1];
+                    curr[j]= max(bought,notBought);
+                }
+                else{
+                    int notSell=prev[j];
+                    int sell=arr[i]+prev[j-1];
+                    curr[j]= max(sell,notSell); 
+                }
+            }
+            prev=curr;
+        }
+        return prev[2*k];
+    }
+};
