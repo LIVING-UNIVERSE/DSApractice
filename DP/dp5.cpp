aab426c1e86e4d98cb1b349aa1fd2299354d0fe7
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-18 21:52:10 +0530

    Longest Increasing Subsequence(O(nlogn))

--- DP/dp5.cpp
+++ DP/dp5.cpp
@@ -92,3 +92,59 @@ class Solution {
         return prev[0];
     }
 };
+
+
+
+//300. Longest Increasing Subsequence
+Solved
+Medium
+Topics
+Companies
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+
+
+
+// this is  O(nlogn) solution if asked specifically then give this approach 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+        // this is a O(nlogn) solution 
+        int n=arr.size();
+        vector<int> v;
+        v.push_back(arr[0]);
+        for(int i=1; i<n ; i++){
+            if(arr[i]>v[v.size()-1]){
+                v.push_back(arr[i]);
+            }
+            else{
+                int ind=lower_bound(v.begin(),v.end(),arr[i])-v.begin();
+                v[ind]=arr[i];
+            }
+        }
+        return v.size();
+    }
+};
