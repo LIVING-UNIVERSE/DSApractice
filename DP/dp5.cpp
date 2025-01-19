5c4d7e2648e1adb7e8736fa52dc2772261da3445
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-20 01:46:22 +0530

    Longest Bitonic subsequence

--- DP/dp5.cpp
+++ DP/dp5.cpp
@@ -500,3 +500,64 @@ public:
         return res;
     }
 };
+
+
+
+//Longest Bitonic subsequence
+Difficulty: MediumAccuracy: 47.34%Submissions: 103K+Points: 4
+Given an array of positive integers. Find the maximum length of Bitonic subsequence. 
+A subsequence of array is called Bitonic if it is first strictly increasing, then strictly decreasing. Return the maximum length of bitonic subsequence.
+ 
+Note : A strictly increasing or a strictly decreasing sequence should not be considered as a bitonic sequence
+
+Examples :
+
+Input: n = 5, nums[] = [1, 2, 5, 3, 2]
+Output: 5
+Explanation: The sequence {1, 2, 5} is increasing and the sequence {3, 2} is decreasing so merging both we will get length 5.
+Input: n = 8, nums[] = [1, 11, 2, 10, 4, 5, 2, 1]
+Output: 6
+Explanation: The bitonic sequence {1, 2, 10, 4, 2, 1} has length 6.
+Input: n = 5, nums[] = [10, 20, 30]
+Output: 0
+Explanation: The decreasing or increasing part cannot be empty
+Input: n = 3, nums[] = [10, 10, 10]
+Output: 0
+Explanation: The subsequences must be strictly increasing or decreasing
+Constraints:
+1 ≤ length of array ≤ 103
+1 ≤ arr[i] ≤ 104
+
+
+
+// solution
+class Solution {
+  private:
+    vector<int> f(vector<int> &arr){
+        int n=arr.size();
+        vector<int> dp(n,1);
+        for(int i=1; i<n; i++){
+            for(int j=0;j<i; j++){
+                if(arr[j]<arr[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp;
+    }
+  public:
+    int LongestBitonicSequence(int n, vector<int> &arr) {
+        vector<int> dpi =f(arr);
+        vector<int> v=arr;
+        reverse(v.begin(),v.end());
+        vector<int> dpd =f(v);
+        reverse(dpd.begin(),dpd.end());
+        int res=0;
+        for(int i=0; i<n ;i++){
+            if(dpi[i]>1 && dpd[i]>1){
+                res=max(res,dpi[i]+dpd[i]-1);
+            }
+        }
+        return res;
+    }
+};
