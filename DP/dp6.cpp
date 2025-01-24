4255fd90024272f162d1cd8da8aaa55e81345952
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-24 23:48:42 +0530

    Burst Balloons

--- DP/dp6.cpp
+++ DP/dp6.cpp
@@ -164,3 +164,99 @@ public:
         return dp[1][arr.size()-2];
     }
 };
+
+
+
+
+//312. Burst Balloons
+Solved
+Hard
+Topics
+Companies
+Google
+Microsoft
+Apple
+Bloomberg
+Uber
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+0 <= nums[i] <= 100
+
+
+// top down approach
+class Solution {
+private:
+     int f(int i,int j,vector<int> &arr,vector<vector<int>> &dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int res=INT_MIN;
+        for(int k=i; k<=j;k++){
+            int val=arr[k]*arr[i-1]*arr[j+1] + f(i,k-1,arr,dp)+f(k+1,j,arr,dp);
+            res=max(res,val);
+        }
+        return dp[i][j]= res;
+     }
+public:
+    int maxCoins(vector<int>& arr) {
+        arr.push_back(1);
+        arr.insert(arr.begin(),1);
+        vector<vector<int>> dp(arr.size(),vector<int>(arr.size(),-1));
+        return f(1,arr.size()-2,arr,dp);
+    }
+};
+
+// bottoms up approach
+class Solution {
+private:
+     int f(int i,int j,vector<int> &arr,vector<vector<int>> &dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int res=INT_MIN;
+        for(int k=i; k<=j;k++){
+            int val=arr[k]*arr[i-1]*arr[j+1] + f(i,k-1,arr,dp)+f(k+1,j,arr,dp);
+            res=max(res,val);
+        }
+        return dp[i][j]= res;
+     }
+public:
+    int maxCoins(vector<int>& arr) {
+        arr.push_back(1);
+        arr.insert(arr.begin(),1);
+        int n=arr.size();
+        vector<vector<int>> dp(arr.size(),vector<int>(arr.size(),0));
+        for(int i=n-2; i>=1;i--){
+            for(int j=i;j<=n-2; j++){
+                int res=INT_MIN;
+                for(int k=i; k<=j; k++){
+                    int val=arr[k]*arr[i-1]*arr[j+1] +dp[i][k-1] + dp[k+1][j];
+                    res=max(res,val);
+                }
+                dp[i][j]=res;
+            }
+        }
+        return dp[1][n-2];
+    }
+};
