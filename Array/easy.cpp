3a3d7e721c4eb5b183a53f2f7b97b66d28ede67a
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-29 19:42:09 +0530

    Union of 2 Sorted with Duplicates

--- Array/easy.cpp
+++ Array/easy.cpp
@@ -252,3 +252,91 @@ public:
         }
     }
 };
+
+
+
+//Union of 2 Sorted with Duplicates
+Difficulty: MediumAccuracy: 31.39%Submissions: 396K+Points: 4
+Given two sorted arrays a[] and b[], where each array may contain duplicate elements , the task is to return the elements in the union of the two arrays in sorted order.
+
+Union of two arrays can be defined as the set containing distinct common elements that are present in either of the arrays.
+Examples:
+
+Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3, 6, 7]
+Output: 1 2 3 4 5 6 7
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+Input: a[] = [2, 2, 3, 4, 5], b[] = [1, 1, 2, 3, 4]
+Output: 1 2 3 4 5
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5.
+Input: a[] = [1, 1, 1, 1, 1], b[] = [2, 2, 2, 2, 2]
+Output: 1 2
+Explanation: Distinct elements including both the arrays are: 1 2.
+Constraints:
+1  <=  a.size(), b.size()  <=  105
+-109  <=  a[i] , b[i]  <=  109
+
+
+
+// brute force
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        set<int> s;
+        for(int i=0; i<a.size();i++){
+            s.insert(a[i]);
+        }
+        for(int i=0; i<b.size();i++){
+            s.insert(b[i]);
+        }
+        vector<int> res;
+        for(auto x:s){
+            res.push_back(x);
+        }
+        return res;
+    }
+};
+
+
+// optimal solution
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        vector<int> v;
+        int i=0,j=0;
+        while(i<a.size()&& j<b.size()){
+            if(a[i]>b[j]){
+                if(v.empty() || v[v.size()-1]!=b[j]) v.push_back(b[j]);
+                j++;
+            }
+            else if(a[i]== b[j]){
+                if(v.empty() || v[v.size()-1]!=b[j]) v.push_back(b[j]);
+                j++;i++;
+            }
+            else{
+                if(v.empty() || v[v.size()-1]!=a[i]) v.push_back(a[i]);
+                i++;
+            }
+        }
+        if(i==a.size()){
+            while(j<b.size()){
+                if(v[v.size()-1]!=b[j]){
+                    v.push_back(b[j]);
+                }
+                j++;
+            }
+        }
+        if(j==b.size()){
+            while(i<a.size()){
+                if(v[v.size()-1]!=a[i]){
+                    v.push_back(a[i]);
+                }
+                i++;
+            }
+        }
+        return v;
+    }
+};
