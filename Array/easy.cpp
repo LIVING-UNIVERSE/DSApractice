23cafe9deab5eaf8fe4a2dcbd91e08fd6abe0c36
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-29 23:27:08 +0530

    Single Number

--- Array/easy.cpp
+++ Array/easy.cpp
@@ -453,3 +453,63 @@ public:
         return res;
     }
 };
+
+
+
+
+//136. Single Number
+Solved
+Easy
+Topics
+Companies
+Hint
+Amazon
+Microsoft
+Bloomberg
+Google
+Adobe
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+
+// optimal solution
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int n=arr.size();
+        int num=arr[0];
+        for(int i=1; i<n;i++){
+            num^=arr[i];
+        }
+        return num;
+    }
+};
