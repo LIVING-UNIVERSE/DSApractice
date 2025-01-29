b8e3fe60cac58fc8b7f2a1afcb53fad041478079
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-01-29 13:00:25 +0530

    Move Zeroes

--- Array/easy.cpp
+++ Array/easy.cpp
@@ -179,3 +179,76 @@ public:
     }
 };
 
+
+
+
+//283. Move Zeroes
+Solved
+Easy
+Topics
+Companies
+Hint
+Facebook
+Microsoft
+Apple
+Amazon
+Yandex
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+
+
+// brute force
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        // brute force solution
+        int n=arr.size();
+        vector<int> temp;
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0) temp.push_back(arr[i]);
+        }
+        for(int i=0; i<n; i++){
+            if(i<temp.size()) arr[i]=temp[i];
+            else{
+                arr[i]=0;
+            }
+        }
+    }
+};
+
+
+// optimal solution
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n=arr.size();
+        int ind=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0){
+                swap(arr[i],arr[ind]);
+                ind++;
+            }
+        }
+    }
+};
