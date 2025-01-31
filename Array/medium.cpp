6ee33334bd81c7fa8f3a193ece5a259b0a98f10d
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-01 04:39:53 +0530

    Majority Element

--- Array/medium.cpp
+++ Array/medium.cpp
@@ -159,3 +159,107 @@ public:
         }
     }
 };
+
+
+
+
+//169. Majority Element
+Solved
+Easy
+Topics
+Companies
+Amazon
+Microsoft
+Adobe
+Apple
+Google
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+// time:O(nlogn) and space:O(n) solution
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map<int,int> m;
+        for(int i=0; i<n; i++){
+            m[arr[i]]++;
+        }
+        int res;
+        for(auto it=m.begin(); it!=m.end(); it++){
+            if(it->second > n/2) res=it->first;
+        }
+        return res;
+    }
+};
+
+
+// time:O(nlogn) and space:O(1) better solution
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int ans=arr[0],len=1;
+        for(int i=1; i<n; i++){
+            if(arr[i]!=ans){
+                ans=arr[i];len=1;
+            }
+            else{
+                len++;
+                if(len> n/2) break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// optimal solution(moore's voting algorithm)
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int n=arr.size();
+        int res=arr[0],len=1;
+        for(int i=1; i<n; i++){
+            if(arr[i]==res){
+                len++;
+            }
+            else if(len==0){
+                res=arr[i];
+            }
+            else{
+                len--;
+            }
+        }
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==res) count++;
+        }
+        if(count> n/2) return res;
+        return -1;
+    }
+};
