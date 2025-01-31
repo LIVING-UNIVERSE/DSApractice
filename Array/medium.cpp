f6c2da28a7958928b54e2098384d3d8e1b61ea31
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-01 03:31:48 +0530

    Dutch National Flag Algorithm(Sort Colors)

--- Array/medium.cpp
+++ Array/medium.cpp
@@ -74,3 +74,88 @@ public:
         return v;
     }
 };
+
+
+
+//75. Sort Colors
+Solved
+Medium
+Topics
+Companies
+Hint
+Microsoft
+Amazon
+VMware
+Adobe
+Salesforce
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+ 
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+
+// brute force
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n=arr.size();
+        int prev=0,next=n-1;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                swap(arr[i],arr[prev]);
+                prev++;
+            }
+        }
+        for(int i=n-1; i>=0;i--){
+            if(arr[i]==2){
+                swap(arr[i],arr[next]);
+                next--;
+            }
+        }
+    }
+};
+
+
+//optimal solution
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n=arr.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[mid],arr[low]);
+                low++;mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
+    }
+};
