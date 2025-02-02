6fab1fc53effe8136fca12bd7a47dfd5c8a106ce
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-03 02:43:23 +0530

    Array Leaders

--- Array/medium.cpp
+++ Array/medium.cpp
@@ -391,3 +391,51 @@ public:
         return f(0,n-1,arr);
     }
 };
+
+
+
+//Array Leaders
+Difficulty: EasyAccuracy: 29.94%Submissions: 810K+Points: 2
+You are given an array arr of positive integers. Your task is to find all the leaders in the array. An element is considered a leader if it is greater than or equal to all elements to its right. The rightmost element is always a leader.
+
+Examples:
+
+Input: arr = [16, 17, 4, 3, 5, 2]
+Output: [17, 5, 2]
+Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+Input: arr = [10, 4, 2, 4, 1]
+Output: [10, 4, 4, 1]
+Explanation: Note that both of the 4s are in output, as to be a leader an equal element is also allowed on the right. side
+Input: arr = [5, 10, 20, 40]
+Output: [40]
+Explanation: When an array is sorted in increasing order, only the rightmost element is leader.
+Input: arr = [30, 10, 10, 5]
+Output: [30, 10, 10, 5]
+Explanation: When an array is sorted in non-increasing order, all elements are leaders.
+Constraints:
+1 <= arr.size() <= 106
+0 <= arr[i] <= 106
+
+
+
+//optimal solution  
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> v;
+        v.push_back(arr[n-1]);
+        int prev=arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            if(arr[i]>=prev){
+                v.push_back(arr[i]);
+                prev=arr[i];
+            }
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+
+
