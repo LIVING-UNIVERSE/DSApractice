4c6ab422d1e2f2a15deb8b333b7d3a5c6b6390c0
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-03 19:26:36 +0530

    Next Permutation

--- Array/medium.cpp
+++ Array/medium.cpp
@@ -583,3 +583,87 @@ public:
     }
 };
 
+
+
+//31. Next Permutation
+Solved
+Medium
+Topics
+Companies
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+
+// brute force
+//(1) genearate all permutations using recursion
+//(2) sort all permutations
+//(3) find the next permutation
+// just tell the interviewer he don't ask you to code this because its code is time comsuming
+
+// optimal solution
+// using stl
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        next_permutation(arr.begin(),arr.end());
+    }
+};
+
+
+// optimal solution
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        int n=arr.size();
+        int lind=n-1;
+        for(int i=n-2; i>=0; i--){
+            if(arr[i]<arr[lind]){
+                for(int j=n-1; j>=0;j--){
+                    if(arr[j]>arr[i]){
+                        swap(arr[i],arr[j]);
+                        break;
+                    }
+                }
+                reverse(arr.begin()+i+1,arr.end());
+                break;
+            }
+            else{
+                lind=i;
+            }
+        }
+        if(lind==0){
+            reverse(arr.begin(),arr.end());
+        }
+    }
+};
+
+
