2951a1f141996ed685ebeb2e9ce7e8774812cdae
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-04 13:34:50 +0530

    Subarray Sum Equals K

--- Array/medium.cpp
+++ Array/medium.cpp
@@ -667,3 +667,76 @@ public:
 };
 
 
+
+
+560. Subarray Sum Equals K
+Solved
+Medium
+Topics
+Companies
+Hint
+Facebook
+Amazon
+Google
+Microsoft
+Oracle
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+
+
+// brute force
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int target) {
+        int n=arr.size();
+        int count=0;
+        for(int i=0; i<n; i++){
+            int sum=0;
+            for(int j=i; j<n; j++){
+                sum+=arr[j];
+                if(sum==target) count++;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+// optimal solution
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int target) {
+        int n=arr.size();
+        unordered_map<int,int> m;
+        m[0]++;
+        int sum=0,count=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+            if(m.find(sum-target)!=m.end()){
+                count+=m[sum-target];
+            }
+            m[sum]++;
+        }
+        return count;
+    }
+};
