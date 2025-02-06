42c9cb27625c7ea51530c77dde04b4e2661c0940
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-06 20:46:10 +0530

    Merge Intervals

--- Array/hard.cpp
+++ Array/hard.cpp
@@ -53,3 +53,87 @@ public:
         return res;
     }
 };
+
+
+
+//56. Merge Intervals
+Solved
+Medium
+Topics
+Companies
+Facebook
+Amazon
+Google
+Bloomberg
+Salesforce
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+
+
+
+// optimal solution
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        int begin=arr[0][0],end=arr[0][1];
+        for(int i=1; i<n; i++){
+            if(arr[i][0]>end){
+                vector<int> temp;
+                temp.push_back(begin);
+                temp.push_back(end);
+                ans.push_back(temp);
+                begin=arr[i][0];
+                end=arr[i][1];
+            }
+            else{
+                end=max(end,arr[i][1]);
+            }
+        }
+        vector<int> temp;
+        temp.push_back(begin);
+        temp.push_back(end);
+        ans.push_back(temp);
+        return ans;
+    }
+};
+
+// further optimal solution
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
+            if(ans.empty() || (arr[i][0]>ans.back()[1])){
+                ans.push_back({arr[i][0],arr[i][1]});
+            }
+            else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
+            }
+        }
+        return ans;
+    }
+};
