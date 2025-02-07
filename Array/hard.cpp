ae9a50b71c1baf65299d061f55a8485785dad259
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-07 21:56:31 +0530

    Merge Without Extra Space

--- Array/hard.cpp
+++ Array/hard.cpp
@@ -137,3 +137,86 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+//Merge Without Extra Space
+Difficulty: MediumAccuracy: 32.01%Submissions: 285K+Points: 4Average Time: 20m
+Given two sorted arrays a[] and b[] of size n and m respectively, the task is to merge them in sorted order without using any extra space. Modify a[] so that it contains the first n elements and modify b[] so that it contains the last m elements.
+
+Examples:
+
+Input: a[] = [2, 4, 7, 10], b[] = [2, 3]
+Output:
+2 2 3 4
+7 10
+Explanation: After merging the two non-decreasing arrays, we get, 2 2 3 4 7 10
+Input: a[] = [1, 5, 9, 10, 15, 20], b[] = [2, 3, 8, 13]
+Output:
+1 2 3 5 8 9
+10 13 15 20
+Explanation: After merging two sorted arrays we get 1 2 3 5 8 9 10 13 15 20.
+Input: a[] = [0, 1], b[] = [2, 3]
+Output:
+0 1
+2 3
+Explanation: After merging two sorted arrays we get 0 1 2 3.
+Constraints:
+1 <= a.size(), b.size() <= 105
+0 <= a[i], b[i] <= 107
+
+
+// optimal solution 1
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        int n=a.size(),m=b.size();
+        int i=n-1,j=0;
+        while(i>=0 && j<m){
+            if(a[i]>b[j]){
+                swap(a[i],b[j]);
+                i--;j++;
+            }
+            else{
+                break;
+            }
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+    }
+};
+
+
+// optimal approach 2(gap method using shell sort)
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        int n=a.size(),m=b.size();
+        int gap=(m+n+1)/2;
+        while(gap>0){
+            int left=0,right=gap;
+            while(right<m+n){
+                if(left<n && right>=n){
+                    if(a[left]>b[right-n]){
+                        swap(a[left],b[right-n]);
+                    }
+                }
+                else if(left<n && right<n){
+                    if(a[left]>a[right]){
+                        swap(a[left],a[right]);
+                    }
+                }
+                else{
+                    if(b[left-n]>b[right-n]){
+                        swap(b[left-n],b[right-n]);
+                    }
+                }
+                left++;right++;
+            }
+            if(gap==1) {break;}
+            gap=(gap+1)/2;
+        }
+    }
+};
