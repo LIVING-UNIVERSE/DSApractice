ff7f7b268370f7cbed327db674d7c2b91a5645df
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-14 22:56:52 +0530

    Two numbers with odd occurrences

--- BitManipulation/bit.cpp
+++ BitManipulation/bit.cpp
@@ -263,3 +263,72 @@ class Solution {
   };
 
 
+
+
+//Two numbers with odd occurrences
+Difficulty: MediumAccuracy: 49.49%Submissions: 70K+Points: 4
+Given an unsorted array, Arr[] of size N and that contains even number of occurrences for all numbers except two numbers. Find the two numbers in decreasing order which has odd occurrences.
+
+Example 1:
+
+Input:
+N = 8
+Arr = {4, 2, 4, 5, 2, 3, 3, 1}
+Output: {5, 1} 
+Explanation: 5 and 1 have odd occurrences.
+
+Example 2:
+
+Input:
+N = 8
+Arr = {1 7 5 7 5 4 7 4}
+Output: {7, 1}
+Explanation: 7 and 1 have odd occurrences.
+
+Your Task:
+You dont need to read input or print anything. Your task is to complete the function twoOddNum() which takes the array Arr[] and its size N as input parameters and returns the two numbers in decreasing order which have odd occurrences.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+2 ≤ N ≤ 106
+1 ≤ Arri ≤ 1012
+
+
+// bit manipulation
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int arr[], long long int n)  
+    {
+        long long c=0;
+        for(long long i=0; i<n ;i++){
+            c=c^arr[i];
+        }
+        long long bitCount=0;
+        while((c&(1<<bitCount))==0){
+            bitCount++;
+        }
+        long long zero=0,one=0;
+        for(long long i=0; i<n; i++){
+            if(arr[i]&(1<<bitCount)){
+                one=one^arr[i];
+            }
+            else{
+                zero=zero^arr[i];
+            }
+        }
+        if(zero>one){
+            return {zero,one};
+        }
+        return {one,zero};
+    }
+};
+
+
+
+
+
+
