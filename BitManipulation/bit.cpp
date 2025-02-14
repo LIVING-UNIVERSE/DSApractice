9adac2e4010c2a1184e253deb1deebede51d6fcd
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-14 22:29:35 +0530

    Find XOR of numbers from L to R.

--- BitManipulation/bit.cpp
+++ BitManipulation/bit.cpp
@@ -217,3 +217,49 @@ class Solution {
 
 
 
+
+
+
+//Find XOR of numbers from L to R.
+Difficulty: EasyAccuracy: 50.0%Submissions: 52K+Points: 2Average Time: 10m
+You are given two integers L and R, your task is to find the XOR of elements of the range [L, R].
+
+Example:
+
+Input: 
+L = 4, R = 8 
+Output:
+8 
+Explanation:
+4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
+Your Task:
+
+Your task is to complete the function findXOR() which takes two integers l and r and returns the XOR of numbers from l to r.
+
+Expected Time Complexity: O(1).
+
+Expected Auxiliary Space: O(1).
+
+Constraints:
+
+1<=l<=r<=109
+
+
+
+// bit manipulation
+class Solution {
+    private:
+      int f(int n){
+          if(n%4==0) return n;
+          if(n%4==1) return 1;
+          if(n%4==2) return n+1;
+          return 0;
+      }
+    public:
+      int findXOR(int l, int r) {
+          l=f(l-1);r=f(r);
+          return l^r;
+      }
+  };
+
+
