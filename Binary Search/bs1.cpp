7917c2ad5a91426a10b6ba75f5cfb8084951d8da
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-16 23:27:10 +0530

    Number of occurrence

--- Binary Search/bs1.cpp
+++ Binary Search/bs1.cpp
@@ -204,3 +204,180 @@ class Solution {
           return low;
       }
   };
+
+
+
+
+
+//34. Find First and Last Position of Element in Sorted Array
+Solved
+Medium
+Topics
+Companies
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+
+
+// brute force
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& arr, int target) {
+            int n=arr.size();
+            int start=-1,end=-1;
+            for(int i=0; i<n; i++){
+                if(arr[i]==target && start==-1){
+                    start=i;
+                }
+                if(arr[i]==target && start!=-1){
+                    end=i;
+                } 
+            }
+            return {start,end};
+        }
+};
+
+
+
+// binary search using stl O(logn)
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& arr, int target) {
+            auto it1=lower_bound(arr.begin(),arr.end(),target);
+            auto it2=upper_bound(arr.begin(),arr.end(),target);
+            if(it1==arr.end()) return {-1,-1};
+            int lb=(*(it1)!=target)?-1:it1-arr.begin();
+            if(lb==-1) return {-1,-1};
+            int ub=it2-1-arr.begin();
+            return {lb,ub};
+        }
+};
+
+
+
+// using binary search O(logn)
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& arr, int target) {
+            int n=arr.size();
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(arr[mid]>=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            if(low==n || arr[low]!=target) return {-1,-1};
+            int lb=low;
+            low=0;high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(arr[mid]==target){
+                    low=mid+1;
+                }
+                else if(arr[mid]>target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            int ub=high;
+            return {lb,ub};
+        }
+};
+
+
+// dekh hamesha low ya high answer return kr denge but wo thode complex ho jata hai toh ans variable use kr aur jb bhi arr[mid]==target ho
+// toh ans=mid krke store karta ja 
+// niche wale ques main wahi kiya hai
+
+
+
+//Number of occurrence
+Difficulty: EasyAccuracy: 59.34%Submissions: 298K+Points: 2Average Time: 20m
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+Output: 0
+Explanation: target = 4 is not present in the given array so the output is 0.
+Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+Output: 3
+Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ target ≤ 106
+
+
+// using binary search O(logn)
+class Solution {
+    public:
+      int countFreq(vector<int>& arr, int target) {
+          int n=arr.size();
+          int low=0,high=n-1,lb=-1;
+          while(low<=high){
+              int mid=(low+high)/2;
+              if(arr[mid]==target){
+                  lb=mid;
+                  high=mid-1;
+              }
+              else if(arr[mid]>target){
+                  high=mid-1;
+              }
+              else{
+                  low=mid+1;
+              }
+          }
+          if(lb==-1) return 0;
+          low=0;high=n-1;
+          int ub=-1;
+          while(low<=high){
+              int mid=(low+high)/2;
+              if(arr[mid]==target){
+                  ub=mid;
+                  low=mid+1;
+              }
+              else if(arr[mid]>target){
+                  high=mid-1;
+              }
+              else{
+                  low=mid+1;
+              }
+          }
+          return ub-lb+1;
+      }
+};
