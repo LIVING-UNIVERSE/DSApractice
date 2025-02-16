531d7aa8129493bd63d7d1b0fb17e0c63bba322c
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-16 17:50:26 +0530

    lower bound and upper bound

--- Binary Search/bs1.cpp
+++ Binary Search/bs1.cpp
@@ -72,3 +72,49 @@ class Solution {
             return f(arr.size()-1,0,target,arr);
         }
     };
+
+
+
+
+
+// lower bound and upper bound
+// lower bound
+class Solution {
+    public:
+        int searchInsert(vector<int>& arr, int target) {
+            int n=arr.size();
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(arr[mid]==target) return mid;
+                else if(arr[mid]<target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1
+                }
+            }
+            return high;
+        }
+};    
+
+
+// upper bound
+class Solution {
+    public:
+        int searchInsert(vector<int>& arr, int target) {
+            int n=arr.size();
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(arr[mid]==target) return mid;
+                else if(arr[mid]>target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            return low;
+        }
+    };
