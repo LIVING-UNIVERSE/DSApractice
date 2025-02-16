74bf06105493fb7b2dd3ec89d0cfe87b390191ce
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-16 20:24:35 +0530

    Implement Lower Bound

--- Binary Search/bs1.cpp
+++ Binary Search/bs1.cpp
@@ -71,50 +71,87 @@ class Solution {
         int search(vector<int>& arr, int target) {
             return f(arr.size()-1,0,target,arr);
         }
-    };
+};
 
 
 
 
-
-// lower bound and upper bound
-// lower bound
-class Solution {
-    public:
-        int searchInsert(vector<int>& arr, int target) {
-            int n=arr.size();
-            int low=0,high=n-1;
-            while(low<=high){
-                int mid=(low+high)/2;
-                if(arr[mid]==target) return mid;
-                else if(arr[mid]<target){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1
-                }
-            }
-            return high;
-        }
-};    
+// Implement Lower Bound
+Easy
+40/40
+Average time to solve is 20m
+Contributed by
+347 upvotes
+Problem statement
+You are given an array 'arr' sorted in non-decreasing order and a number 'x'. You must return the index of the lower bound of 'x'.
 
 
-// upper bound
-class Solution {
-    public:
-        int searchInsert(vector<int>& arr, int target) {
-            int n=arr.size();
-            int low=0,high=n-1;
-            while(low<=high){
-                int mid=(low+high)/2;
-                if(arr[mid]==target) return mid;
-                else if(arr[mid]>target){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            return low;
-        }
-    };
+
+Note:
+1. For a sorted array 'arr', 'lower_bound' of a number 'x' is defined as the smallest index 'idx' such that the value 'arr[idx]' is not less than 'x'.If all numbers are smaller than 'x', then 'n' should be the 'lower_bound' of 'x', where 'n' is the size of array.
+
+2. Try to do this in O(log(n)).
+
+
+Example:
+Input: ‘arr’ = [1, 2, 2, 3] and 'x' = 0
+
+Output: 0
+
+Explanation: Index '0' is the smallest index such that 'arr[0]' is not less than 'x'.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= ‘n’ <= 10^5
+0 <= ‘arr[i]’ <= 10^5
+1 <= ‘x’ <= 10^5
+Sample Input 1:
+6
+1 2 2 3 3 5
+0
+
+
+Sample Output 1:
+0
+
+
+Explanation Of Sample Input 1 :
+Index '0' is the smallest index such that 'arr[0]' is not less than 'x'.
+
+
+Sample Input 2:
+6
+1 2 2 3 3 5
+2
+
+
+Sample Output 2:
+1
+
+
+Sample Input 3:
+6
+1 2 2 3 3 5
+7
+
+
+Sample Output 3:
+6
+
+
+
+// solution
+int lowerBound(vector<int> arr, int n, int x) {
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return low;
+}
