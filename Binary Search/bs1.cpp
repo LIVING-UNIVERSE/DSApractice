ebd527f1d89b8f668c90abe6cd6e53304775dd58
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-18 04:03:25 +0530

    Single Element in a Sorted Array

--- Binary Search/bs1.cpp
+++ Binary Search/bs1.cpp
@@ -671,3 +671,98 @@ class Solution {
             return ans;
         }
 };
+
+
+
+
+//540. Single Element in a Sorted Array
+Solved
+Medium
+Topics
+Companies
+Amazon
+Facebook
+Microsoft
+Google
+Apple
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+
+
+// brute force O(n/2)
+class Solution {
+    public:
+        int singleNonDuplicate(vector<int>& arr) {
+            int n=arr.size();
+            int low=0,high=n-1;
+            while(low<high){
+                if(arr[low+1]==arr[low] && arr[high-1]==arr[high]){
+                    low+=2;high-=2;
+                }
+                else if(arr[low+1]!=arr[low]){
+                    return arr[low];
+                }
+                else{
+                    return arr[high];
+                }
+            }
+            return arr[low];
+        }
+};
+
+
+// optimal O(logn)
+class Solution {
+    public:
+        int singleNonDuplicate(vector<int>& arr) {
+            int n=arr.size();
+            if (n==1) return arr[0];
+            if(arr[0]!=arr[1]) return arr[0];
+            if(arr[n-1]!=arr[n-2]) return arr[n-1];
+            int low=2,high=n-3;
+            int mid;
+            while(low<=high){
+                mid=(low+high)/2;
+                if(arr[mid-1]!=arr[mid] && arr[mid+1]!=arr[mid]){
+                    break;
+                }
+                else if(arr[mid-1]==arr[mid]){
+                    if(mid%2==0){
+                        high=mid-1;
+                    }
+                    else{
+                        low=mid+1;
+                    }
+                }
+                else{
+                    if(mid%2==0){
+                        low=mid+1;
+                    }
+                    else{
+                        high=mid-1;
+                    }
+                }
+            }
+            return arr[mid];
+        }
+};
