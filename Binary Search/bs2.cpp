313fb759476aaac69f2c03dce889c435eb38fbf3
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-25 20:14:42 +0530

    Split Array Largest Sum

--- Binary Search/bs2.cpp
+++ Binary Search/bs2.cpp
@@ -464,4 +464,77 @@ class Solution {
 
 
 
+//410. Split Array Largest Sum
+Solved
+Hard
+Topics
+Companies
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the largest sum among the two subarrays is only 18.
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 106
+1 <= k <= min(50, nums.length)
+
+
+// optimal solution (binary search)
+class Solution {
+    private:
+        bool f(vector<int> &arr,long long val, int k){
+            long long sum=arr[0];
+            int n=arr.size(),count=1;
+            for(int i=1; i<n; i++){
+                long long lsum=sum+arr[i];
+                if(lsum<=val){
+                    sum+=arr[i];
+                }
+                else{
+                    sum=arr[i];
+                    count++;
+                }
+            }
+            return (count<=k);
+        }
+    public:
+        int splitArray(vector<int>& arr, int k) {
+            int n=arr.size();
+            long long  low=*max_element(arr.begin(),arr.end());
+            long long  high=accumulate(arr.begin(),arr.end(),0);
+            while(low<=high){
+                long long  mid=low+(high-low)/2;
+                bool val=f(arr,mid,k);
+                if(val){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            return low;
+        }
+};
+
+
+
 
