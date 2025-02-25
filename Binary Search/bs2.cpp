83d7ef3f91d8976885774251a71d2b36e5363392
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-25 17:22:38 +0530

    Aggressive Cows

--- Binary Search/bs2.cpp
+++ Binary Search/bs2.cpp
@@ -321,4 +321,71 @@ class Solution {
             }
             return low;
         }
-    };
+};
+
+
+
+
+//Aggressive Cows
+Difficulty: MediumAccuracy: 59.57%Submissions: 114K+Points: 4Average Time: 30m
+You are given an array with unique elements of stalls[], which denote the position of a stall. You are also given an integer k which denotes the number of aggressive cows. Your task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
+
+Examples :
+
+Input: stalls[] = [1, 2, 4, 8, 9], k = 3
+Output: 3
+Explanation: The first cow can be placed at stalls[0], 
+the second cow can be placed at stalls[2] and 
+the third cow can be placed at stalls[3]. 
+The minimum distance between cows, in this case, is 3, which also is the largest among all possible ways.
+Input: stalls[] = [10, 1, 2, 7, 5], k = 3
+Output: 4
+Explanation: The first cow can be placed at stalls[0],
+the second cow can be placed at stalls[1] and
+the third cow can be placed at stalls[4].
+The minimum distance between cows, in this case, is 4, which also is the largest among all possible ways.
+Input: stalls[] = [2, 12, 11, 3, 26, 7], k = 5
+Output: 1
+Explanation: Each cow can be placed in any of the stalls, as the no. of stalls are exactly equal to the number of cows.
+The minimum distance between cows, in this case, is 1, which also is the largest among all possible ways.
+Constraints:
+2 <= stalls.size() <= 106
+0 <= stalls[i] <= 108
+2 <= k <= stalls.size()
+
+
+
+// optimal solution (binary search)
+class Solution {
+    private:
+      bool f(vector<int> &arr, int k, int dist){
+          int prev=arr[0],count=1;
+          int n=arr.size();
+          int i=1;
+          while(i<n){
+              if(arr[i]-prev>=dist){
+                  count++;
+                  prev=arr[i];
+              }
+              i++;
+          }
+          return (count>=k);
+          
+      }
+    public:
+      int aggressiveCows(vector<int> &arr, int k) {
+          sort(arr.begin(),arr.end());
+          int low=1,high=*max_element(arr.begin(),arr.end());
+          while(low<=high){
+              int mid=low+(high-low)/2;
+              bool val=f(arr,k,mid);
+              if(val){
+                  low=mid+1;
+              }
+              else{
+                  high=mid-1;;
+              }
+          }
+          return high;
+      }
+  };
