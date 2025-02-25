ec526d4e9478c410a97ef61c4ae51d029fa31eea
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-25 22:23:32 +0530

    The Painter's Partition Problem-II

--- Binary Search/bs2.cpp
+++ Binary Search/bs2.cpp
@@ -538,3 +538,63 @@ class Solution {
 
 
 
+//The Painter's Partition Problem-II
+Difficulty: HardAccuracy: 27.52%Submissions: 129K+Points: 8
+Dilpreet wants to paint his dogs home that has n boards with different lengths. The length of ith board is given by arr[i] where arr[] is an array of n integers. He hired k painters for this work and each painter takes 1 unit time to paint 1 unit of the board.
+
+Return the minimum time to get this job done if all painters start together with the constraint that any painter will only paint continuous boards, say boards numbered [2,3,4] or only board [1] or nothing but not boards [2,4,5].
+
+Examples:
+
+Input: arr[] = [5, 10, 30, 20, 15], k = 3
+Output: 35
+Explanation: The most optimal way will be: Painter 1 allocation : [5,10], Painter 2 allocation : [30], Painter 3 allocation : [20,15], Job will be done when all painters finish i.e. at time = max(5+10, 30, 20+15) = 35
+Input: arr[] = [10, 20, 30, 40], k = 2
+Output: 60
+Explanation: The most optimal way to paint: Painter 1 allocation : [10,20,30], Painter 2 allocation : [40], Job will be complete at time = 60
+Input: arr[] = [100, 200, 300, 400], k = 1
+Output: 1000
+Explanation: There is only one painter, so the painter must paint all boards sequentially. The total time taken will be the sum of all board lengths, i.e., 100 + 200 + 300 + 400 = 1000.
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+1 ≤ k ≤ 105
+
+
+//  optimal solution (binary search)
+class Solution {
+    private:
+      bool f(vector<int> & arr,long long val ,int k){
+          long long sum=arr[0];
+          int n=arr.size();
+          int count=1;
+          for(int i=1;i<n; i++){
+              long long lsum=arr[i]+sum;
+              if(lsum<=val){
+                  sum+=arr[i];
+              }
+              else{
+                  count++;
+                  sum=arr[i];
+              }
+          }
+          return (count<=k);
+      }
+    public:
+      int minTime(vector<int>& arr, int k) {
+          int n=arr.size();
+          long long low=*max_element(arr.begin(),arr.end());
+          long long high=accumulate(arr.begin(),arr.end(),0);
+          while(low<=high){
+              long long mid=low+(high-low)/2;
+              bool val=f(arr,mid,k);
+              if(val){
+                  high=mid-1;
+              }
+              else{
+                  low=mid+1;
+              }
+          }
+          return low;
+      }
+  };
