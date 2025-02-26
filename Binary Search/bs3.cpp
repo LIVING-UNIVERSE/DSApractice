746f3835e5bd95d39ef19a794eb7a0fb5ff82bb1
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-27 03:13:16 +0530

    Search a 2D Matrix

--- Binary Search/bs3.cpp
+++ Binary Search/bs3.cpp
@@ -69,4 +69,111 @@ class Solution {
           }
           return ind;
       }
-  };
+};
+
+
+
+//74. Search a 2D Matrix
+Solved
+Medium
+Topics
+Companies
+Facebook
+Amazon
+Microsoft
+Bloomberg
+Apple
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+
+
+// brute force O(m*n)
+class Solution {
+    public:
+      bool searchMatrix(vector<vector<int>> &matrix, int target) {
+          // brute force
+          int n=matrix.size(),m=matrix[0].size();
+          for(int i=0;i<n; i++){
+              for(int j=0;j<m;j++){
+                  if(matrix[i][j]==target) return true;
+              }
+          }
+          return false;
+      }
+};
+
+
+// better solution t:o(n)+O(log(m))
+class Solution {
+    public:
+      bool searchMatrix(vector<vector<int>> &matrix, int target) {
+          // better O(n)+O(log(m))
+          int n=matrix.size(),m=matrix[0].size();
+          int row=-1;
+          for(int i=0;i<n; i++){
+              if(matrix[i][0]<=target && matrix[i][m-1]>=target){
+                  row=i;
+                  break;
+              }
+          }
+          if(row==-1) return false;
+          int low=0,high=m-1;
+          while(low<=high){
+              int mid=(low+high)/2;
+              if(matrix[row][mid]==target) return true;
+              else if(matrix[row][mid]<target) low=mid+1;
+              else high=mid-1;
+          }
+          return false;
+      }
+};
+
+
+// optimal solution O(log(m*n))
+class Solution {
+    public:
+        bool searchMatrix(vector<vector<int>>& arr, int target) {
+            int n=arr.size(),m=arr[0].size();
+            int low=0,high=n*m-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                int div=mid/m,rem=mid%m;
+                if(arr[div][rem]==target){
+                    return true;
+                }
+                else if(arr[div][rem]<target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            return false;
+        }
+};
