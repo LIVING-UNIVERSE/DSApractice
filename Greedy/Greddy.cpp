81d988b89f3a798bf9fd700112c15caec38b198f
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-28 00:46:24 +0530

    Jump Game

--- Greedy/Greddy.cpp
+++ Greedy/Greddy.cpp
@@ -316,8 +316,8 @@ int main() {
 
 
 
-/*
-678. Valid Parenthesis String
+
+//678. Valid Parenthesis String
 Solved
 Medium
 Topics
@@ -433,7 +433,79 @@ public:
 
 
 
+//55. Jump Game
+Solved
+Medium
+Topics
+Companies
+Amazon
+Facebook
+Apple
+DoorDash
+Flipkart
+You are given an integer array nums. You are initially positioned at the array''s first index, and each element in the array represents your maximum jump length at that position.
 
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+
+
+// solution 1 t:O(n) s:O(1)
+class Solution {
+    public:
+        bool canJump(vector<int>& arr) {
+            int n=arr.size();
+            if(n==1) return true;
+            int maxIndex=arr[0];
+            for(int i=1; i<n; i++){
+                if(i>maxIndex) return false;
+                if(maxIndex>=n-1) return true;
+                if(i+arr[i]>maxIndex){
+                    maxIndex=i+arr[i];
+                }
+            }
+            return true;
+        }
+};
+
+
+// another solution t:O(n) s:O(1)
+class Solution {
+    public:
+        bool canJump(vector<int>& arr) {
+            int n=arr.size();
+            if(n==1) return true;
+            int maxJump=-1;
+            for(int i=0; i<n; i++){
+                if(arr[i]>=maxJump){
+                    maxJump=arr[i];
+                }
+                else{
+                    maxJump--;
+                }
+                if(i<n-1 && maxJump==0) return false;
+            }
+            return true;
+        }
+};
 
 
 
