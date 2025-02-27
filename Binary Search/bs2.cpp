beb894c2cbf19ffc98a86070558722da8c6bacdb
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-02-27 20:30:11 +0530

    K-th element of two Arrays

--- Binary Search/bs2.cpp
+++ Binary Search/bs2.cpp
@@ -827,3 +827,94 @@ class Solution {
             return 0;
         }
 };
+
+
+
+
+//K-th element of two Arrays
+Difficulty: MediumAccuracy: 37.4%Submissions: 323K+Points: 4Average Time: 15m
+Given two sorted arrays a[] and b[] and an element k, the task is to find the element that would be at the kth position of the combined sorted array.
+
+Examples :
+
+Input: a[] = [2, 3, 6, 7, 9], b[] = [1, 4, 8, 10], k = 5
+Output: 6
+Explanation: The final combined sorted array would be [1, 2, 3, 4, 6, 7, 8, 9, 10]. The 5th element of this array is 6.
+Input: a[] = [100, 112, 256, 349, 770], b[] = [72, 86, 113, 119, 265, 445, 892], k = 7
+Output: 256
+Explanation: Combined sorted array is [72, 86, 100, 112, 113, 119, 256, 265, 349, 445, 770, 892]. The 7th element of this array is 256.
+Constraints:
+
+1 <= a.size(), b.size() <= 106
+1 <= k <= a.size() + b.size()
+0 <= a[i], b[i] < 108
+
+
+
+// brute force O(n+m)
+class Solution {
+    public:
+      int kthElement(vector<int>& a, vector<int>& b, int k) {
+          int n=a.size(),m=b.size();
+          int i=0,j=0;
+          int prev=-1,count=0;
+          while(i<n && j<m){
+              if(a[i]<b[j]){
+                  prev=a[i];
+                  i++;count++;
+              }
+              else{
+                  prev=b[j];
+                  j++;count++;
+              }
+              if(count==k) return prev;
+          }
+          count++;
+          if(j>=m){
+              while(count<k){
+                  i++;count++;
+              }
+              return a[i];
+          }
+          if(i>=n){
+              while(count<k){
+                  j++;count++;
+              }
+              return b[j];
+          }
+          return -1;
+      }
+  };
+
+
+  // optimal solution O(log(min(n,m)))
+  class Solution {
+    public:
+      int kthElement(vector<int>& a, vector<int>& b, int k) {
+          int n=a.size(),m=b.size();
+          int low=max(0,k-m),high=min(n,k);
+          while(low<=high){
+              int mid1=(low+high)/2;
+              int mid2=k-mid1;
+              int l1=INT_MIN,l2=INT_MIN;
+              int r1=INT_MAX,r2=INT_MAX;
+              if(mid1<n) r1=a[mid1];
+              if(mid2<m) r2=b[mid2];
+              if(mid1-1>=0) l1=a[mid1-1];
+              if(mid2-1>=0) l2=b[mid2-1];
+              if(l2>r1){
+                  low=mid1+1;
+              }
+              else if(r2>=l1 && r1>=l2){
+                  return max(l1,l2);
+              }
+              else{
+                  high=mid1-1;
+              }
+          }
+          return 0;
+      }
+  };
+
+
+  
