792a399ac4ad0e0c1c1519e7f9274a0704033928
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-02 02:41:29 +0530

    Longest Substring Without Repeating Characters

---
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -0,0 +1,98 @@
+//3. Longest Substring Without Repeating Characters
+Solved
+Medium
+Topics
+Companies
+Hint
+Amazon
+Microsoft
+Facebook
+Bloomberg
+Apple
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
+
+// brute force O(n^2)
+class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            int n=s.size();
+            if(n==0) return 0;
+            if(n==1) return 1;
+            int res=0;
+            for(int i=0; i<n; i++){
+                vector<int> hash(256,-1);
+                int length=0;
+                for(int j=i;j<n;j++){
+                    int val=s[j];
+                    if(hash[val]==-1){
+                        length++;
+                        hash[val]=j;
+                    }
+                    else{
+                        res=max(res,length);
+                        break;
+                    }
+                }
+                res=max(res,length);
+            }
+            return res;
+        }
+};
+
+
+// optimal solution O(n)
+// use constant space of 256 instead of a map
+class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            int n=s.size();
+            if(n==0) return 0;
+            unordered_map<char,int> m;
+            int res=0,start=0,length=1;
+            m[s[0]]=0;
+            for(int i=1; i<n; i++){
+                if(m.find(s[i])==m.end()){
+                    length++;
+                    m[s[i]]=i;
+                }
+                else{
+                    if(m[s[i]]>=start){
+                        res=max(res,length);
+                        int ind=m[s[i]];
+                        length=i-ind-1;
+                        start=ind+1;
+                    }
+                    length++;
+                    m[s[i]]=i;
+                }
+            }
+            res=max(res,length);
+            return res;
+        }
+};
