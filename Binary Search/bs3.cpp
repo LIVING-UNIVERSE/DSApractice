a7dcf864e93152c76a7e9bbc21126b6a41ba623d
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-01 16:35:18 +0530

    Find a Peak Element II

--- Binary Search/bs3.cpp
+++ Binary Search/bs3.cpp
@@ -282,3 +282,100 @@ class Solution {
             return false;
         }
 };
+
+
+
+//1901. Find a Peak Element II
+Solved
+1472
+Medium
+Topics
+Companies
+Hint
+A peak element in a 2D grid is an element that is strictly greater than all of its adjacent neighbors to the left, right, top, and bottom.
+
+Given a 0-indexed m x n matrix mat where no two adjacent cells are equal, find any peak element mat[i][j] and return the length 2 array [i,j].
+
+You may assume that the entire matrix is surrounded by an outer perimeter with the value -1 in each cell.
+
+You must write an algorithm that runs in O(m log(n)) or O(n log(m)) time.
+
+ 
+
+Example 1:
+
+
+
+Input: mat = [[1,4],[3,2]]
+Output: [0,1]
+Explanation: Both 3 and 4 are peak elements so [1,0] and [0,1] are both acceptable answers.
+Example 2:
+
+
+
+Input: mat = [[10,20,15],[21,30,14],[7,16,32]]
+Output: [1,1]
+Explanation: Both 30 and 32 are peak elements so [1,1] and [2,2] are both acceptable answers.
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 500
+1 <= mat[i][j] <= 105
+No two adjacent cells are equal.
+
+
+// brute force O(m*n)
+class Solution {
+    public:
+        vector<int> findPeakGrid(vector<vector<int>>& arr) {
+            int n=arr.size(),m=arr[0].size();
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    int left=-1,right=-1,top=-1,bottom=-1;
+                    if(j-1>=0) left=arr[i][j-1];    
+                    if(j+1<m) right=arr[i][j+1];
+                    if(i-1>=0) top=arr[i-1][j];
+                    if(i+1<n) bottom=arr[i+1][j];
+                    if(arr[i][j]>left && arr[i][j]>right && arr[i][j]>top && arr[i][j]>bottom){
+                        return {i,j};
+                    }       
+                }
+            }
+            return {};
+        }
+};
+
+
+// better solution O(m*log(n))
+class Solution {
+    public:
+        vector<int> findPeakGrid(vector<vector<int>>& arr) {
+            int n=arr.size(),m=arr[0].size();
+            int low=0,high=m-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                int ind=-1,maxe=-1;
+                for(int i=0; i<n; i++){
+                    if(arr[i][mid]>maxe){
+                        maxe=arr[i][mid];ind=i;
+                    }
+                }
+                int left=-1,right=-1;
+                if(mid-1>=0) left=arr[ind][mid-1];
+                if(mid+1<m) right=arr[ind][mid+1];
+                if(left<arr[ind][mid] && right<arr[ind][mid]){
+                    return {ind,mid};
+                }
+                else if(left>arr[ind][mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            return {-1,-1};
+        }
+};
