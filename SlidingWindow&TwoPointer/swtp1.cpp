cb1207640c948a35e803e39e618806c40b772141
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-02 16:01:16 +0530

    Max Consecutive Ones III

--- SlidingWindow&TwoPointer/swtp1.cpp
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -96,3 +96,117 @@ class Solution {
             return res;
         }
 };
+
+
+
+
+//1004. Max Consecutive Ones III
+Solved
+1656
+Medium
+Topics
+Companies
+Hint
+Facebook
+Microsoft
+Google
+Amazon
+ByteDance
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+
+
+// brute force O(n^2)
+class solution{
+    public:
+        int longestOnes(vector<int>& arr, int k) {
+            int n=arr.size();
+            int res=0;
+            for(int i=0; i<n; i++){
+                int count=0;
+                for(int j=i;j<n;j++){
+                    if(arr[j]==0) count++;
+                    if(count>k){
+                        break;
+                    }
+                }
+                res=max(res,j-i+1);
+            }
+            return res;
+        }
+};
+
+
+// better solution O(n)+O(n)==O(2n)
+//                  ^    ^
+//                  |    |
+//           (for left) (for right)
+class Solution {
+    public:
+        int longestOnes(vector<int>& arr, int k) {
+            int n=arr.size();
+            int l=0,r=0,length=0,maxLength=0,count=0;
+            while(r<n){
+                if(arr[r]==0) count++;
+                if(count<=k){
+                    length=r-l+1;
+                    maxLength=max(length,maxLength);
+                }
+                else{
+                    while(arr[l]==1){
+                        l++;
+                    }
+                    l++;count--;
+                }
+                r++;
+            }
+            return maxLength;
+        }
+};
+
+
+
+
+// opitmal solution O(n)
+// use sliding window
+class Solution {
+    public:
+        int longestOnes(vector<int>& arr, int k) {
+            int n=arr.size();
+            int l=0,r=0,length=0,maxLength=0,count=0;
+            while(r<n){
+                if(arr[r]==0) count++;
+                if(count<=k){
+                    length=r-l+1;
+                    maxLength=max(length,maxLength);
+                }
+                else{
+                   if(arr[l]==0) count--;
+                   l++;
+                }
+                r++;
+            }
+            return maxLength;
+        }
+};
