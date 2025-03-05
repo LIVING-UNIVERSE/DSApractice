eb25831d0459c23fda80701479089006e0741b33
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-05 16:50:07 +0530

    Longest Repeating Character Replacement

--- SlidingWindow&TwoPointer/swtp1.cpp
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -372,4 +372,156 @@ class Solution {
           }
           return maxLength;
       }
-  };
+};
+
+
+
+
+//424. Longest Repeating Character Replacement
+Solved
+Medium
+Topics
+Companies
+Google
+Amazon
+Uber
+Adobe
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+
+
+
+// brute force 
+// t:O(n^2) sp:O(1)
+class Solution {
+    public:
+        int characterReplacement(string s, int k) {
+            int n=s.size();
+            if(n==1) return 1;
+            int maxLength=0;
+            for(int i=0; i<n; i++){
+                int length=0;
+                for(int j=i;j<n;j++){
+                    int count=0;
+                    int maxCount=0;
+                    for(int l=i;l<=j;l++){
+                        if(s[l]!=s[j]){
+                            count++;
+                        }
+                        maxCount=max(maxCount,count);
+                    }
+                    if(maxCount<=k){
+                        length=j-i+1;
+                        maxLength=max(maxLength,length);
+                    }
+                }
+            }
+            return maxLength;
+        }
+};
+
+
+// better solution
+// t:(O(n)+O(n))*26 sp:O(1)
+class Solution {
+    public:
+        int characterReplacement(string s, int k) {
+            int n=s.size();
+            int l=0,r=0,maxLength=0,maxFreq=0;
+            vector<int> hash(26,0);
+            while(r<n){
+                hash[s[r]-'A']++;
+                if(hash[s[r]-'A']>maxFreq){
+                    maxFreq=hash[s[r]-'A'];
+                }
+                int reqOp=r-l+1-maxFreq;
+                while(reqOp>k){
+                    hash[s[l]-'A']--;
+                    l++;
+                    maxFreq=*max_element(hash.begin(),hash.end());
+                    reqOp=r-l+1-maxFreq;
+                }
+                maxLength=max(r-l+1,maxLength);
+                r++;
+            }
+            return maxLength;
+        }
+};
+
+
+// better solution
+// t:O(n)+O(n) sp:O(1)
+class Solution {
+    public:
+        int characterReplacement(string s, int k) {
+            int n=s.size();
+            int l=0,r=0,maxLength=0,maxFreq=0;
+            vector<int> hash(26,0);
+            while(r<n){
+                hash[s[r]-'A']++;
+                if(hash[s[r]-'A']>maxFreq){
+                    maxFreq=hash[s[r]-'A'];
+                }
+                int reqOp=r-l+1-maxFreq;
+                while(reqOp>k){
+                    hash[s[l]-'A']--;
+                    l++;
+                    reqOp=r-l+1-maxFreq;
+                }
+                maxLength=max(r-l+1,maxLength);
+                r++;
+            }
+            return maxLength;
+        }
+};
+
+
+
+// optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int characterReplacement(string s, int k) {
+            int n=s.size();
+            int l=0,r=0,maxLength=0,maxFreq=0;
+            vector<int> hash(26,0);
+            while(r<n){
+                hash[s[r]-'A']++;
+                if(hash[s[r]-'A']>maxFreq){
+                    maxFreq=hash[s[r]-'A'];
+                }
+                int reqOp=r-l+1-maxFreq;
+                if(reqOp>k){
+                    hash[s[l]-'A']--;
+                    l++;
+                    reqOp=r-l+1-maxFreq;
+                }
+                maxLength=max(r-l+1,maxLength);
+                r++;
+            }
+            return maxLength;
+        }
+};
