f973ced8495b300154deb7d78c60d8c3a590262d
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-06 20:37:16 +0530

    Binary Subarrays With Sum

--- SlidingWindow&TwoPointer/swtp1.cpp
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -525,3 +525,133 @@ class Solution {
             return maxLength;
         }
 };
+
+
+
+
+//930. Binary Subarrays With Sum
+Solved
+1592
+Medium
+Topics
+Companies
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+nums[i] is either 0 or 1.
+0 <= goal <= nums.length
+
+
+// brute force
+// t:O(n^2) sp:O(1)
+class Solution {
+    public:
+        int numSubarraysWithSum(vector<int>& arr, int goal) {
+            int n=arr.size();
+            int res=0;
+            for(int i=0; i<n; i++){
+                int count=0,sum=0;
+                for(int j=i;j<n;j++){
+                    sum+=arr[j];
+                    if(sum==goal) count++;
+                }
+                res+=count;
+            }
+            return res;
+        }
+};
+
+// better solution
+// t:O(n)+O(n) sp:O(n)
+class Solution {
+    public:
+        int numSubarraysWithSum(vector<int>& arr, int goal) {
+            int n=arr.size();
+            int sum=0,count=0;
+            unordered_map<int,int> m;
+            for(int i=0; i<n; i++){
+                sum+=arr[i];
+                if(sum==goal){
+                    count++;
+                }
+                if(m.find(sum-goal)!=m.end()){
+                    count+=m[sum-goal];
+                }
+                m[sum]++;
+            }
+            return count;
+        }
+};
+
+// optimal solution 1
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int numSubarraysWithSum(vector<int>& arr, int goal) {
+            int n=arr.size();
+            int l=0,r=0,sum=0,count=0,res=0;
+            while(r<n){
+                sum+=arr[r];
+                while(l<r && (arr[l]==0 || sum>goal)){
+                    if(arr[l]==0){
+                        count++;
+                    }
+                    else{
+                        count=0;
+                        sum-=arr[l];
+                    }
+                    l++;
+                }
+                if(sum==goal){
+                    res+=1+count;
+                }
+                r++;
+            }
+            return res;
+        }
+};
+
+
+// optimal solution 2
+class Solution {
+    private:
+        int f(vector<int> &arr,int goal){
+            int n=arr.size();
+            if(goal<0) return 0;
+            int l=0,r=0,sum=0,res=0;
+            while(r<n){
+                sum+=arr[r];
+                while(sum>goal){
+                    sum-=arr[l];l++;
+                }
+                res+=r-l+1;
+                r++;
+            }
+            return res;
+        }
+    public:
+        int numSubarraysWithSum(vector<int>& arr, int goal) {
+            return f(arr,goal)-f(arr,goal-1);
+        }
+};
