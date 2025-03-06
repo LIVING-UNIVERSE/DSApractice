6cd8f1d2316bfccd725c73515520fa8f59020075
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-06 20:46:52 +0530

    Count Number of Nice Subarrays

--- SlidingWindow&TwoPointer/swtp1.cpp
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -655,3 +655,123 @@ class Solution {
             return f(arr,goal)-f(arr,goal-1);
         }
 };
+
+
+
+//1248. Count Number of Nice Subarrays
+Solved
+1624
+Medium
+Topics
+Companies
+Hint
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+ 
+
+Constraints:
+
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10^5
+1 <= k <= nums.length
+
+
+// brute force
+// t:O(n^2) sp:O(1)
+class Solution {
+    public:
+        int numberOfSubarrays(vector<int>& arr, int k) {
+            int n=arr.size();
+            int res=0;
+            for(int i=0; i<n; i++){
+                int count=0;
+                for(int j=i;j<n;j++){
+                    if(arr[j]%2!=0){
+                        count++;
+                    }
+                    if(count==k){
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+};
+
+
+// optimal solution 1
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int numberOfSubarrays(vector<int>& arr, int k) {
+            int n=arr.size();
+            int l=0,r=0,count=0,res=0,evenCount=0;
+            while(r<n){
+                if(arr[r]%2!=0){
+                    count++;
+                }
+                while(l<r &&(arr[l]%2==0 || count>k)){
+                    if(arr[l]%2==0){
+                        evenCount++;
+                    }
+                    else{
+                        count--;evenCount=0;
+                    }
+                    l++;
+                }
+                if(count==k){
+                    res+=1+evenCount;
+                }
+                r++;
+            }
+            return res;
+        }
+};
+
+
+
+// optimal solution 2
+// t:O(n) sp:O(1)
+class Solution {
+    private:
+        int f(vector<int> &arr, int k){
+            int n=arr.size(); 
+            int l=0,r=0,count=0,res=0;
+            while(r<n){
+                if(arr[r]%2!=0){
+                    count++;
+                }
+                while(count>k){
+                    if(arr[l]%2!=0){
+                        count--;
+                    }
+                    l++;
+                }
+                res+=r-l+1;
+                r++;
+            }
+            return res;
+        }
+    public:
+        int numberOfSubarrays(vector<int>& arr, int k) {
+            return f(arr,k)-f(arr,k-1);
+        }
+};
