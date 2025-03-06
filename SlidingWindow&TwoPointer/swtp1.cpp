68f32f0a8871a7040b9b1b261aef9a80927317bd
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-06 23:51:25 +0530

    Number of Substrings Containing All Three Characters

--- SlidingWindow&TwoPointer/swtp1.cpp
+++ SlidingWindow&TwoPointer/swtp1.cpp
@@ -775,3 +775,87 @@ class Solution {
             return f(arr,k)-f(arr,k-1);
         }
 };
+
+
+
+
+
+//1358. Number of Substrings Containing All Three Characters
+Solved
+1646
+Medium
+Topics
+Companies
+Hint
+Microsoft
+Adobe
+DE Shaw
+Given a string s consisting only of characters a, b and c.
+
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ 
+
+Example 1:
+
+Input: s = "abcabc"
+Output: 10
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+Example 2:
+
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+Example 3:
+
+Input: s = "abc"
+Output: 1
+ 
+
+Constraints:
+
+3 <= s.length <= 5 x 10^4
+s only consists of a, b or c characters.
+
+
+
+// brute force
+// t:O(n^2) sp:O(1)
+class Solution {
+    public:
+        int numberOfSubstrings(string s) {
+            int n=s.size();
+            int res=0;
+            for(int i=0; i<n; i++){
+                for(int j=i+2;j<n;j++){
+                    unordered_map<char,int> m;
+                    for(int k=i;k<=j;k++){
+                        m[s[k]]++;
+                    }
+                    if(m.find('a')!=m.end() && m.find('b')!=m.end() && m.find('c')!=m.end()){
+                        res++;
+                    }
+                }
+            }
+        }
+};
+
+
+// Optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int numberOfSubstrings(string s) {
+            int n=s.size();
+            int l=0,r=0,res=0;
+            vector<int> hash(3,-1);
+            while(r<n){
+                hash[s[r]-'a']=r;
+                if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                    res+=*min_element(hash.begin(),hash.end())+1;
+                }
+                r++;
+            }
+            return res;
+        }
+};
