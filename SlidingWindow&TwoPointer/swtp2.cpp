be186ae6e60dd335502473111939ecb3e163e0e4
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-07 02:47:48 +0530

    Subarrays with K Different Integers

--- SlidingWindow&TwoPointer/swtp2.cpp
+++ SlidingWindow&TwoPointer/swtp2.cpp
@@ -101,3 +101,70 @@ class Solution {
           return maxLength;
       }
   };
+
+
+
+
+//992. Subarrays with K Different Integers
+Solved
+2210
+Hard
+Topics
+Companies
+Hint
+Given an integer array nums and an integer k, return the number of good subarrays of nums.
+
+A good array is an array where the number of different integers in that array is exactly k.
+
+For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,2,3], k = 2
+Output: 7
+Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+Example 2:
+
+Input: nums = [1,2,1,3,4], k = 3
+Output: 3
+Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+1 <= nums[i], k <= nums.length
+
+
+
+// optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    private:
+        int f(vector<int> &arr, int k){
+            int n=arr.size();
+            if(k<=0) return 0;
+            int l=0,r=0,res=0;
+            unordered_map<int,int> m;
+            while(r<n){
+                m[arr[r]]++;
+                while(m.size()>k){
+                    m[arr[l]]--;
+                    if(m[arr[l]]==0){
+                        m.erase(arr[l]);
+                    }
+                    l++;
+                }
+                res+=r-l+1;
+                r++;
+            }
+            return res;
+        }
+    public:
+        int subarraysWithKDistinct(vector<int>& arr, int k) {
+            return f(arr,k)-f(arr,k-1);
+        }
+};
