7afbb07dd04a256078733d45d5edc7a291e3d74c
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-08 19:31:36 +0530

    Longest Common Prefix

--- Strings/easy.cpp
+++ Strings/easy.cpp
@@ -226,3 +226,54 @@ class Solution {
             return arr.substr(0,i+1);
         }
 };
+
+
+
+//14. Longest Common Prefix
+Solved
+Easy
+Topics
+Companies
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+
+
+// solution
+class Solution {
+    public:
+        string longestCommonPrefix(vector<string>& arr) {
+            string str=arr[0];
+            for(int i=1; i<arr.size();i++){
+                if(arr[i].size()<str.size()) str=arr[i];
+            }
+            for(int i=0; i<str.size(); i++){
+                for(int j=0;j<arr.size(); j++){
+                    if(str[i]!=arr[j][i]){
+                        if(i==0) return "";
+                        return str.substr(0,i);
+                    }
+                }
+            }
+            return str;
+        }
+    };
