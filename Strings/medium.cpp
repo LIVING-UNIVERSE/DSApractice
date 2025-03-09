2a1a61202a107ec03c7fea00e1097d57042b7bb2
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-09 23:08:41 +0530

    Roman to Integer

--- Strings/medium.cpp
+++ Strings/medium.cpp
@@ -184,3 +184,91 @@ class Solution {
             return res;
         }
 };
+
+
+
+//13. Roman to Integer
+Solved
+Easy
+Topics
+Companies
+Hint
+Amazon
+Adobe
+Apple
+Google
+Bloomberg
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+ 
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+// solution
+class Solution {
+    private:
+        int f(char c){
+            if(c=='I') return 1;
+            if(c=='V') return 5;
+            if(c=='X') return 10;
+            if(c=='L') return 50;
+            if(c=='C') return 100;
+            else if(c=='D') return 500;
+            return 1000;
+        }
+    public:
+        int romanToInt(string s) {
+            int x=0,y=0;
+            int val=0;
+            for(int i=s.size()-1; i>=0;i--){
+                int x=f(s[i]);
+                if(x>=y){
+                    val+=x;
+                }
+                else{
+                    val-=x;
+                }
+                y=x;
+            }
+            return val;
+        }
+};
