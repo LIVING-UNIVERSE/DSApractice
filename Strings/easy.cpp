4e21c26df979a4ed811478c82246c0a3a0539afd
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-09 13:40:11 +0530

    Valid Anagram

--- Strings/easy.cpp
+++ Strings/easy.cpp
@@ -276,4 +276,70 @@ class Solution {
             }
             return str;
         }
-    };
+};
+
+
+
+// 242. Valid Anagram
+Solved
+Easy
+Topics
+Companies
+Facebook
+Bloomberg
+Amazon
+Spotify
+Microsoft
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+
+// brute force
+// t:O(nlogn) sp:O(1)
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+            return (s==t);
+        }
+};
+
+// optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            if(s.size()!=t.size()) return false;
+            vector<int> h1(26,0);
+            vector<int> h2(26,0);
+            for(int i=0; i<s.size(); i++){
+                h1[s[i]-'a']++;h2[t[i]-'a']++;
+            }
+            return h1==h2;
+        }
+};
+
