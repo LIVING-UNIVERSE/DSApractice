38f0f1c3ba939c0599578136fb2a98d0369b2245
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-11 19:51:20 +0530

    Reverse Linked List

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -52,4 +52,93 @@ class Solution {
             }
             return slow;
         }
-    };
+};
+
+
+
+
+
+//206. Reverse Linked List
+Solved
+Easy
+Topics
+Companies
+Amazon
+Microsoft
+Apple
+Bloomberg
+Facebook
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+
+// iterative solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL) return NULL;
+        ListNode *prev=head;
+        ListNode* curr=head->next;
+        head->next=NULL;
+        while(curr!=NULL){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+};
+
+
+
+
+class Solution {
+    private:
+        ListNode* f(ListNode* node,ListNode* prev){
+            if(node==NULL) return prev;
+            ListNode* temp=node->next;
+            node->next=prev;
+            return f(temp,node);
+        }
+    public:
+        ListNode* reverseList(ListNode* head) {
+            return f(head,NULL);
+        }
+};
