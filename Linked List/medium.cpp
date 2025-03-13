0a6ed773a19bbb5965bc4b1261cd50f6607ffbd9
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-13 14:36:14 +0530

    Palindrome Linked List

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -646,3 +646,104 @@ class Solution {
             return head;
         }
 };
+
+
+
+
+//234. Palindrome Linked List
+Solved
+Easy
+Topics
+Companies
+Facebook
+Amazon
+Microsoft
+Apple
+Bloomberg
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+
+
+// brute force
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        bool isPalindrome(ListNode* head) {
+            vector<int> v;
+            ListNode* curr=head;
+            while(curr!=NULL){
+                v.push_back(curr->val);
+                curr=curr->next;
+            }
+            vector<int> temp=v;
+            reverse(temp.begin(),temp.end());
+            return temp==v;
+        }
+};
+
+
+// optimal solution
+// t:O(n) s:O(1)
+class Solution {
+    private:
+        ListNode* reverse(ListNode* head){
+            ListNode* curr=head;
+            ListNode* prev=NULL;
+            while(curr!=NULL){
+                ListNode* temp=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=temp;
+            }
+            return prev;
+        }
+    public:
+        bool isPalindrome(ListNode* head){
+            ListNode* curr=head;
+            ListNode* slow=head;
+            ListNode* fast=head;
+            while(fast!=NULL && fast->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+            }
+            ListNode* nHead= reverse(slow);
+            ListNode* temp=nHead;
+            while(temp!=NULL){
+                if(temp->val!=curr->val)  return false;
+                temp=temp->next;
+                curr=curr->next;
+            }
+            reverse(nHead);
+            return true;
+        }
+};
