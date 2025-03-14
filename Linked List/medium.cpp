239ba4c68d1c86d7a5f9f6a0002ba10054626f70
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-14 17:15:09 +0530

    Add Two Numbers

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -1302,3 +1302,83 @@ class Solution {
           return head;
       }
 };
+
+
+
+
+// 2. Add Two Numbers
+Solved
+Medium
+Topics
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+
+// solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            ListNode* curr1=l1;
+            ListNode* curr2=l2;
+            ListNode* newHead= new ListNode(-1);
+            ListNode* temp=newHead;
+            int carry=0;
+            while(curr1!=NULL || curr2!=NULL){
+                int sum=carry;
+                if(curr1){
+                    sum+=curr1->val;
+                    curr1=curr1->next;
+                }
+                if(curr2){
+                    sum+=curr2->val;
+                    curr2=curr2->next;
+                }
+                ListNode* create= new ListNode(sum%10);
+                carry=sum/10;
+                temp->next=create;
+                temp=temp->next;
+            }
+            if(carry){
+                ListNode* one = new ListNode(1);
+                temp->next=one;
+            }
+            return newHead->next;
+        }
+};
