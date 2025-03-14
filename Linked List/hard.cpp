f5d11bc6841efaaadd479d2450b070ff30e2e2c8
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-14 21:40:58 +0530

    Rotate List

--- Linked List/hard.cpp
+++ Linked List/hard.cpp
@@ -88,3 +88,78 @@ class Solution {
             return dummy->next;
         }
 };
+
+
+
+//61. Rotate List
+Solved
+Medium
+Topics
+Companies
+Microsoft
+Bloomberg
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+
+
+// solution
+// t:O(n) s:O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    private:
+        int len(ListNode* head){
+            int count=0;
+            while(head!=NULL){
+                count++;head=head->next;
+            }
+            return count;
+        }
+    public:
+        ListNode* rotateRight(ListNode* head, int k) {
+            if(head==NULL) return NULL;
+            int length=len(head);
+            k=k%length;
+            if(k==0) return head;
+            ListNode* curr= head;
+            int val=length-k-1;
+            while(val--){
+                curr=curr->next;
+            }
+            ListNode* newHead=curr->next;
+            curr->next=NULL;
+            curr=newHead;
+            while(curr->next!=NULL){
+                curr=curr->next;
+            }
+            curr->next=head;
+            return newHead;
+        }
+};
