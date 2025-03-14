3c67457e0eeef9b99bcf353b311961d8be6a12fa
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-15 02:06:58 +0530

    Remove duplicates from a sorted doubly linked list

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -1542,3 +1542,82 @@ public:
         return v;
     }
 };
+
+
+
+//Remove duplicates from a sorted doubly linked list
+Difficulty: EasyAccuracy: 50.36%Submissions: 54K+Points: 2Average Time: 10m
+Given a doubly linked list of n nodes sorted by values, the task is to remove duplicate nodes present in the linked list.
+
+Example 1:
+
+Input:
+n = 6
+1<->1<->1<->2<->3<->4
+Output:
+1<->2<->3<->4
+Explanation:
+Only the first occurance of node with value 1 is 
+retained, rest nodes with value = 1 are deleted.
+Example 2:
+
+Input:
+n = 7
+1<->2<->2<->3<->3<->4<->4
+Output:
+1<->2<->3<->4
+Explanation:
+Only the first occurance of nodes with values 2,3 and 4 are 
+retained, rest repeating nodes are deleted.
+Your Task:
+You have to complete the method removeDuplicates() which takes 1 argument: the head of the linked list.  Your function should return a pointer to a linked list with no duplicate element.
+
+Constraint:
+1 <= n <= 105
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(1)
+
+
+
+// optimal solution
+// t:O(n) s:O(1)    
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+
+class Solution
+{
+public:
+
+    Node * removeDuplicates(struct Node *head){
+        Node* curr=head->next;
+        while(curr!=NULL){
+            if(curr->prev->data==curr->data){
+                Node* pre= curr->prev;
+                Node* post=curr->next;
+                curr->prev=NULL;curr->next=NULL;
+                pre->next=post;
+                if(post){
+                    post->prev=pre;
+                }
+                curr=post;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+};
