21154748e2b13db8449fda3dceaa4df1a27fb70b
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-15 01:48:39 +0530

    Find pairs with given sum in doubly linked list

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -1438,3 +1438,107 @@ class Solution {
           return dummy->next;
       }
 };
+
+
+//Find pairs with given sum in doubly linked list
+Difficulty: EasyAccuracy: 66.01%Submissions: 74K+Points: 2Average Time: 10m
+Given a sorted doubly linked list of positive distinct elements, the task is to find pairs in a doubly-linked list whose sum is equal to given value target.
+
+ 
+
+Example 1:
+
+Input:  
+1 <-> 2 <-> 4 <-> 5 <-> 6 <-> 8 <-> 9
+target = 7
+Output: (1, 6), (2,5)
+Explanation: We can see that there are two pairs 
+(1, 6) and (2,5) with sum 7.
+ 
+
+Example 2:
+
+Input: 
+1 <-> 5 <-> 6
+target = 6
+Output: (1,5)
+Explanation: We can see that there is one pairs  (1, 5) with sum 6.
+
+ 
+
+Your Task:
+You dont need to read input or print anything. Your task is to complete the function findPairsWithGivenSum() which takes head node of the doubly linked list and an integer target as input parameter and returns an array of pairs. If there is no such pair return empty array.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+Constraints:
+1 <= N <= 105
+1 <= target <= 105
+
+
+// brute force
+// t:O(n) s:O(n)
+
+//User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+        vector<pair<int,int>> v;
+        unordered_set<int> s;
+        Node* curr= head;
+        while(curr!=NULL){
+            if(s.find(target-curr->data)!=s.end()){
+                v.push_back({target-curr->data,curr->data});
+            }
+            s.insert(curr->data);
+            curr=curr->next;
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+
+
+
+// optimal solution
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+        if(head==NULL|| head->next==NULL) return {};
+        Node* tail=head;
+        while(tail->next!=NULL){
+            tail=tail->next;
+        }
+        Node* curr=head;
+        vector<pair<int,int>> v;
+        while(curr!=tail && curr->prev!=tail){
+            if(curr->data+tail->data==target){
+                v.push_back({curr->data,tail->data});
+                curr=curr->next;
+                tail=tail->prev;
+            }
+            else if(curr->data+tail->data>target){
+                tail=tail->prev;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return v;
+    }
+};
