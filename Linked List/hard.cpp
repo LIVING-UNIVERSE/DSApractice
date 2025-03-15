8921640c54765e7217450b15291ec862f79466c1
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-15 23:36:23 +0530

    Flattening a Linked List

--- Linked List/hard.cpp
+++ Linked List/hard.cpp
@@ -163,3 +163,124 @@ class Solution {
             return newHead;
         }
 };
+
+
+
+
+//Flattening a Linked List
+Difficulty: MediumAccuracy: 51.53%Submissions: 178K+Points: 4Average Time: 40m
+Given a linked list containing n head nodes where every node in the linked list contains two pointers:
+(i) next points to the next node in the list.
+(ii) bottom pointer to a sub-linked list where the current node is the head.
+Each of the sub-linked lists nodes and the head nodes are sorted in ascending order based on their data.
+Your task is to flatten the linked list such that all the nodes appear in a single level while maintaining the sorted order.
+
+Note:
+1. ↓ represents the bottom pointer and -> represents the next pointer.
+2. The flattened list will be printed using the bottom pointer instead of the next pointer.
+
+Examples:
+
+Input:
+
+Output: 5-> 7-> 8-> 10 -> 19-> 20-> 22-> 28-> 30-> 35-> 40-> 45-> 50.
+Explanation: 
+Bottom pointer of 5 is pointing to 7.
+Bottom pointer of 7 is pointing to 8.
+Bottom pointer of 8 is pointing to 10 and so on.
+Input:
+ 
+Output: 5-> 7-> 8-> 10-> 19-> 22-> 28-> 30-> 50
+Explanation:
+Bottom pointer of 5 is pointing to 7.
+Bottom pointer of 7 is pointing to 8.
+Bottom pointer of 8 is pointing to 10 and so on.
+Constraints:
+0 <= n <= 100
+1 <= number of nodes in sub-linked list(mi) <= 50
+1 <= node->data <= 104
+
+
+// brute force
+// t:O(n^2) s:O(n)
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    public:
+      // Function which returns the  root of the flattened linked list.
+      Node *flatten(Node *root) {
+          priority_queue<int,vector<int>,greater<int>> pq;
+          Node* curr= root;
+          while(curr!=NULL){
+              Node* temp=curr;
+              while(temp!=NULL){
+                  pq.push(temp->data);
+                  temp=temp->bottom;
+              }
+              curr=curr->next;
+          }
+          Node* dummy= new Node(-1);
+          Node* temp= dummy;
+          while(!pq.empty()){
+              Node* create= new Node(pq.top());
+              temp->bottom=create;
+              temp=temp->bottom;
+              pq.pop();
+          }
+          return dummy->bottom;
+      }
+  };
+
+
+
+// optimal  solution
+class Solution{
+    private:
+      Node* merge(Node* headA,Node* headB){
+          Node* curr1=headA;
+          Node* curr2=headB;
+          Node* dummy= new Node(-1);
+          Node* dum= dummy;
+          while(curr1!=NULL && curr2!=NULL){
+              if(curr1->data<curr2->data){
+                  dum->bottom=curr1;
+                  curr1=curr1->bottom;
+              }
+              else{
+                  dum->bottom=curr2;
+                  curr2=curr2->bottom;
+              }
+              dum=dum->bottom;
+          }
+          dum->bottom=(curr1)?curr1:curr2;
+          Node* newHead= dummy->bottom;
+          delete(dummy);
+          return newHead;
+      }
+    public:
+      // Function which returns the  root of the flattened linked list.
+      Node *flatten(Node *head) {
+          Node* curr=head;
+          while(curr->next){
+              Node* post=curr->next->next;
+              curr=merge(curr,curr->next);
+              curr->next=post;
+          }
+          return curr;
+      }
+};
+  
