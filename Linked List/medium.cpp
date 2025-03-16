a879ec1faa6281e9ee92ca89d321f404f93e9562
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-16 17:15:39 +0530

    Delete all occurrences of a given key in a doubly linked list

--- Linked List/medium.cpp
+++ Linked List/medium.cpp
@@ -1621,3 +1621,92 @@ public:
         return head;
     }
 };
+
+
+
+
+
+//Delete all occurrences of a given key in a doubly linked list
+Difficulty: MediumAccuracy: 50.04%Submissions: 45K+Points: 4Average Time: 30m
+You are given the head_ref of a doubly Linked List and a Key. Your task is to delete all occurrences of the given key if it is present and return the new DLL.
+
+Example1:
+
+Input: 
+2<->2<->10<->8<->4<->2<->5<->2
+2
+Output: 
+10<->8<->4<->5
+Explanation: 
+All Occurences of 2 have been deleted.
+
+Example2:
+
+Input: 
+9<->1<->3<->4<->5<->1<->8<->4
+9
+Output: 
+1<->3<->4<->5<->1<->8<->4
+Explanation: 
+All Occurences of 9 have been deleted.
+Your Task:
+
+Complete the function void deleteAllOccurOfX(struct Node** head_ref, int key), which takes the reference of the head pointer and an integer value key. Delete all occurrences of the key from the given DLL.
+
+Expected Time Complexity: O(N).
+
+Expected Auxiliary Space: O(1).
+
+Constraints:
+
+1<=Number of Nodes<=105
+
+0<=Node Value <=109
+
+
+// solution
+// User function Template for C++
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+class Solution {
+    public:
+      void deleteAllOccurOfX(struct Node** head_ref, int x) {
+          Node* head = *head_ref;
+          Node* curr = head;
+          Node* prev = NULL;
+          
+          // Traverse the linked list
+          while (curr) {
+              if (curr->data == x) {
+                  // Case 1: If the node to delete is the head
+                  if (curr == head) {
+                      head = curr->next;
+                      Node* temp = curr;
+                      curr = curr->next;
+                      delete(temp);
+                  }
+                  // Case 2: If the node is not the head
+                  else {
+                      prev->next = curr->next;
+                      Node* temp = curr;
+                      curr = curr->next;
+                      delete(temp);
+                  }
+              }
+              else {
+                  prev = curr;
+                  curr = curr->next;
+              }
+          }
+          *head_ref = head;
+      }
+};
