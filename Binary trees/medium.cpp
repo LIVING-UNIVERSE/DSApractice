b1d6604fd7d0504a809d92bbb724df953606df58
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-19 12:13:59 +0530

    Binary Tree Zigzag Level Order Traversal

--- Binary trees/medium.cpp
+++ Binary trees/medium.cpp
@@ -367,3 +367,80 @@ class Solution {
 };
 
 
+
+
+
+//103. Binary Tree Zigzag Level Order Traversal
+Solved
+Medium
+Topics
+Companies
+Amazon
+Facebook
+Microsoft
+Bloomberg
+Google
+Given the root of a binary tree, return the zigzag level order traversal of its nodes values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+
+
+
+// optimal solution
+// t:O(n) s:O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        bool flag=true;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> v(size);
+            for(int i=0;i<size;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                int ind=(flag)?i:size-i-1;
+                v[ind]=temp->val;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            ans.push_back(v);
+            flag=(!flag);
+        }
+        return ans;
+    }
+};
