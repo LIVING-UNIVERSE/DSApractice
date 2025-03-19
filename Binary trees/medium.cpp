00113de35c3b219dc8f06b68691aeac6d47b4df5
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-19 08:08:29 +0530

    Binary Tree Maximum Path Sum

--- Binary trees/medium.cpp
+++ Binary trees/medium.cpp
@@ -189,3 +189,78 @@ class Solution {
             return ans;
         }
 };
+
+
+
+//124. Binary Tree Maximum Path Sum
+Solved
+Hard
+Topics
+Companies
+DoorDash
+Amazon
+Facebook
+Microsoft
+Google
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+Example 2:
+
+
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 3 * 104].
+-1000 <= Node.val <= 1000
+
+
+
+// optimal solution
+// the idea is to calculate the maximum path sum for each node and update the value of 'ans' if the sum is greater than 'ans'
+// t: O(n) s: O(h) where h is the height of the tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+        int f(TreeNode* root,int & ans){
+            if(!root) return 0;
+            int l= f(root->left,ans);
+            int r=f(root->right,ans);
+            int sum=root->val;
+            if(l>=0) sum+=l;
+            if(r>=0) sum+=r;
+            ans=max(ans,sum);
+            return max(root->val,root->val+max(l,r));
+        }
+    public:
+        int maxPathSum(TreeNode* root) {
+            int ans=-1e8;
+            f(root,ans);
+            return ans;
+        }
+    };
