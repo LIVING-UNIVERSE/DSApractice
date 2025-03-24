07aa2c43339955222f0fad7d056df62b7be03219
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-24 15:25:29 +0530

    Maximum Width of Binary Tree

--- Binary trees/hard.cpp
+++ Binary trees/hard.cpp
@@ -266,5 +266,106 @@ class Solution {
             if (root->right) sum += root->right->data;
             root->data = sum;
         }
-    };
+};
+
+
+
+
+//662. Maximum Width of Binary Tree
+Solved
+Medium
+Topics
+Companies
+Amazon
+Google
+Microsoft
+Facebook
+Bloomberg
+Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of a 32-bit signed integer.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: 4
+Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
+Example 2:
+
+
+Input: root = [1,3,2,5,null,null,9,6,null,7]
+Output: 7
+Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
+Example 3:
+
+
+Input: root = [1,3,2,5]
+Output: 2
+Explanation: The maximum width exists in the second level with length 2 (3,2).
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 3000].
+-100 <= Node.val <= 100
+
+
+// brute force
+// t:O(n) sp:O(n)
+class Solution {
+    public:
+        int widthOfBinaryTree(TreeNode* root) {
+            if(root==NULL) return 0;
+            queue<pair<TreeNode*,long long>> q;
+            q.push({root,0LL});
+            long long ans=0;
+            while(!q.empty()){
+                int size=q.size();
+                long long sm=q.front().second;
+                long long lg=q.back().second;
+                ans=max(ans,lg-sm+1);
+                for(int i=0;i<size;i++){
+                    auto [temp,ind]= q.front();
+                    q.pop();
+                    if(temp->left) q.push({temp->left,2*ind+1-sm});
+                    if(temp->right) q.push({temp->right,2*ind+2-sm});
+                }
+            }
+            return (int)ans;
+        }
+};
+
+
+// optimal solution
+// t:O(n) sp:O(n)
+class Solution {
+    public:
+        int widthOfBinaryTree(TreeNode* root) {
+            if(root==NULL) return 0;
+            queue<pair<TreeNode*,long long>> q;
+            q.push({root,0LL});
+            long long ans=0;
+            while(!q.empty()){
+                int size=q.size();
+                long long sm=q.front().second;
+                long long lg=q.back().second;
+                ans=max(ans,lg-sm+1);
+                for(int i=0;i<size;i++){
+                    auto [temp,ind]= q.front();
+                    ind=ind-sm;
+                    q.pop();
+                    if(temp->left) q.push({temp->left,2*ind+1});
+                    if(temp->right) q.push({temp->right,2*ind+2});
+                }
+            }
+            return (int)ans;
+        }
+};
     
