242f7e4a0366d110acd47ed03b6ba54fc33d047d
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-26 20:45:06 +0530

    Insert into a Binary Search Tree

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -186,3 +186,73 @@ class Solution{
             return res;
         }
 };
+
+
+
+//701. Insert into a Binary Search Tree
+Solved
+1342
+Medium
+Topics
+Companies
+LinkedIn
+Amazon
+Goldman Sachs
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+Explanation: Another accepted tree is:
+
+Example 2:
+
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+Example 3:
+
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-108 <= Node.val <= 108
+All the values Node.val are unique.
+-108 <= val <= 108
+Its guaranteed that val does not exist in the original BST.
+
+
+
+// optimal  solution
+// t:O(h) sp:O(1)
+class Solution {
+    public:
+        TreeNode* insertIntoBST(TreeNode* root, int val) {
+            TreeNode* temp= new TreeNode(val);
+            if(!root){
+                return temp;
+            }
+            TreeNode* prev=NULL;
+            TreeNode* curr=root;
+            while(curr){
+                prev=curr;
+                if(curr->val>val) curr=curr->left;
+                else{
+                    curr=curr->right;
+                }
+            }
+            if(prev->val>val) prev->left=temp;
+            else{
+                prev->right=temp;
+            }
+            return root;
+        }
+};
