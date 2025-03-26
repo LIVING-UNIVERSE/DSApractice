f5c22ce94cad1163efad50fec99b4e4bb4a5e80f
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-26 20:12:04 +0530

    Floor in BST

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -119,3 +119,70 @@ int findCeil(Node* root, int input) {
     }
     return ans;
 }
+
+
+//Floor in BST
+Difficulty: MediumAccuracy: 51.06%Submissions: 101K+Points: 4Average Time: 20m
+You are given a BST(Binary Search Tree) with n number of nodes and value x. your task is to find the greatest value node of the BST which is smaller than or equal to x.
+Note: when x is smaller than the smallest node of BST then returns -1.
+
+Examples:
+
+Input:
+n = 7               2
+                     \
+                      81
+                    /     \
+                 42       87
+                   \       \
+                    66      90
+                   /
+                 45
+x = 87
+Output: 87
+Explanation: 87 is present in tree so floor will be 87.
+Input:
+n = 4                     6
+                           \
+                            8
+                          /   \
+                        7       9
+x = 11
+Output: 9
+Input:
+n = 4                     6
+                           \
+                            8
+                          /   \
+                        7       9
+x = 5
+Output: -1
+Constraint:
+1 <= Noda data <= 109
+1 <= n <= 105
+
+
+// optimal solution
+// t:O(logn) sp:O(1)
+class Solution{
+
+    public:
+        int floor(Node* root, int x) {
+            int res=-1;
+            Node* curr=root;
+            while(curr){
+                if(curr->data==x){
+                    res=curr->data;
+                    break;
+                }
+                else if(curr->data>x){
+                    curr=curr->left;
+                }
+                else{
+                    res=curr->data;
+                    curr=curr->right;
+                }
+            }
+            return res;
+        }
+};
