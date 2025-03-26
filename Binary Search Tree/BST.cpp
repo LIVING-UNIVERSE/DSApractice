a1fd760b1aaa9dc412b3fd9cf51ebf4256badf38
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-26 20:08:07 +0530

    Ceil in BST

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -56,4 +56,66 @@ class Solution {
             if(right) return right;
             return NULL;
         }
-    };
+};
+
+
+
+//Ceil in BST
+Difficulty: MediumAccuracy: 62.73%Submissions: 133K+Points: 4Average Time: 30m
+Given a BST and a number X, find Ceil of X.
+Note: Ceil(X) is a number that is either equal to X or is immediately greater than X.
+
+If Ceil could not be found, return -1.
+
+Example 1:
+
+Input: root = [5, 1, 7, N, 2, N, N, N, 3], X = 3
+      5
+    /   \
+   1     7
+    \
+     2 
+      \
+       3
+Output: 3
+Explanation: We find 3 in BST, so ceil of 3 is 3.
+Example 2:
+
+Input: root = [10, 5, 11, 4, 7, N, N, N, N, N, 8], X = 6
+     10
+    /  \
+   5    11
+  / \ 
+ 4   7
+      \
+       8
+Output: 7
+Explanation: We find 7 in BST, so ceil of 6 is 7.
+Your task:
+You dont need to read input or print anything. Just complete the function findCeil() to implement ceil in BST which returns the ceil of X in the given BST.
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Value of nodes<= 105
+
+
+// optimal solution
+int findCeil(Node* root, int input) {
+    if (root == NULL) return -1;
+    Node* curr=root;
+    int ans=-1;
+    while(curr){
+        if(curr->data>input){
+            ans=curr->data;
+            curr=curr->left;
+        }
+        else if(curr->data==input){
+            ans=curr->data;
+            break;
+        }
+        else{
+            curr=curr->right;
+        }
+    }
+    return ans;
+}
