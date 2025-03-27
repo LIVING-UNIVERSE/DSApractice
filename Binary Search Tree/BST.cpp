4319bb51c7d76e0a3299869075bde82ef6cae02b
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-28 00:58:56 +0530

    Delete Node in a BST

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -256,3 +256,133 @@ class Solution {
             return root;
         }
 };
+
+
+
+//450. Delete Node in a BST
+Solved
+Medium
+Topics
+Companies
+Amazon
+Microsoft
+Oracle
+Google
+LinkedIn
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and its also accepted.
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+Example 3:
+
+Input: root = [], key = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-105 <= Node.val <= 105
+Each node has a unique value.
+root is a valid binary search tree.
+-105 <= key <= 105
+ 
+
+Follow up: Could you solve it with time complexity O(height of tree)?
+
+
+
+// optimal solution
+// t:O(h) sp:O(1)
+class Solution {
+    private:
+        TreeNode* f(TreeNode* root){
+            if(!root->left) return root->right;
+            else if(!root->right) return root->left;
+            TreeNode* right=root->right;
+            TreeNode* temp=root->left;
+            while(temp->right){
+                temp=temp->right;
+            }
+            temp->right=right;
+            return root->left;
+        }
+    public:
+        TreeNode* deleteNode(TreeNode* root, int key) {
+            if(root==NULL) return NULL;
+            TreeNode* curr=root;
+            while(curr){
+                if(curr->val==key) break;
+                else if(curr->val>key) curr=curr->left;
+                else{
+                    curr=curr->right;
+                }
+            }
+            if(!curr) return root;
+            if(curr==root) return f(root);
+            curr=root;
+            while(curr){
+                if(curr->val>key){
+                    if(curr->left && curr->left->val==key) curr->left=f(curr->left);
+                    else{
+                        curr=curr->left;
+                    }
+                }
+                else{
+                    if(curr->right && curr->right->val==key) curr->right=f(curr->right);
+                    else{
+                        curr=curr->right;
+                    }
+                }
+            }
+            return root;
+        }
+};
+
+
+// optimal solution
+// this is the standard solution
+// t:O(h) sp:O(h)
+class Solution {
+    public:
+        TreeNode* deleteNode(TreeNode* root, int key) {
+            if(root==NULL) return NULL;
+            if(root->val>key){
+                root->left=deleteNode(root->left,key);
+            }
+            else if(root->val<key){
+                root->right=deleteNode(root->right,key);
+            }
+            else{
+                if(root->left==NULL && root->right==NULL) return NULL;
+                if(!root->left) return root->right;
+                if(!root->right) return root->left;
+                TreeNode* temp= root->left;
+                TreeNode* right=root->right;
+                while(temp->right){
+                    temp=temp->right;
+                }
+                temp->right=right;
+                return root->left;
+            }
+            return root;
+        }
+    };
