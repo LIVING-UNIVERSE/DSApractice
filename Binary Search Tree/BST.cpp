c8ef7708326dbed6f7c510b267209fdeac60744b
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-03-31 00:53:32 +0530

    Predecessor and Successor

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -741,3 +741,118 @@ class Solution {
             return f(ind,INT_MAX,preorder);
         }
 };
+
+
+
+
+//Predecessor and Successor
+Difficulty: MediumAccuracy: 47.36%Submissions: 133K+Points: 4
+You are given root node of the BST and an integer key. You need to find the in-order successor and predecessor of the given key. If either predecessor or successor is not found, then set it to NULL.
+
+Note:- In an inorder traversal the number just smaller than the target is the predecessor and the number just greater than the target is the successor. 
+
+Examples :
+
+Input: root[] = [8, 1, 9, N, 4, N, 10, 3, N, N, N]
+      8
+    /   \
+   1     9
+   \      \
+    4      10
+   /
+  3
+key = 8
+Output: 4 9
+Explanation: In the given BST the inorder predecessor of 8 is 4 and inorder successor of 8 is 9.
+Input: root[] = [10, 2, 11, 1, 5, N, N, N, N, 3, 6, N, 4, N, N]
+      10
+    /   \
+   2     11
+  / \
+ 1   5
+    / \
+   3   6
+    \
+     4
+key = 11
+Output: 10 -1
+Explanation: In given BST, the inorder predecessor of 11 is 10 whereas it does not have any inorder successor.
+Input: root[] = [2, 1, 3]
+      2
+    /   \
+   1     3
+key = 3
+Output: 2 -1
+Constraints: 
+1 <= Number of nodes <= 105
+1 <= key <= 106
+
+
+// brute force
+// t:O(n) sp:O(n)
+class Solution{
+    private:
+        void f(Node* root,vector<Node*> &v){
+            if(root==NULL) return ;
+            f(root->left,v);
+            v.push_back(root);
+            f(root->right,v);
+        }
+    public:
+        void findPreSuc(Node* root, Node*& pre, Node*& suc, int key){
+            vector<Node*> v;
+            f(root,v);
+            int n=v.size();
+            for(int i=0;i<n;i++){
+                if(v[i]->key<key){
+                    pre=v[i];
+                }
+                if(v[i]->key>key){
+                    suc=v[i];
+                    break;
+                }
+            }
+        }
+};
+
+
+
+// optimal solution
+// O(logn) sp:O(1)
+class Solution{
+    private:
+        Node* findPre(Node*root,int key){
+            Node* curr=root;
+            Node* res=NULL;
+            while(curr){
+                if(curr->key<key){
+                    res=curr;
+                    curr=curr->right;
+                }
+                else{
+                    curr=curr->left;
+                }
+            }
+            return res;
+        }
+        Node* findSuc(Node* root,int key){
+            Node* curr=root;
+            Node* res=NULL;
+            while(curr){
+                if(curr->key>key){
+                    res=curr;
+                    curr=curr->left;
+                }
+                else{
+                    curr=curr->right;
+                }
+            }
+            return res;
+        }
+    public:
+        void findPreSuc(Node* root, Node*& pre, Node*& suc, int key){
+            pre=findPre(root,key);
+            suc=findSuc(root,key);
+        }
+    };
+    
