2e9565356d5168aaaf357042007aac000c290ee5
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-01 22:17:04 +0530

    Recover Binary Search Tree

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -1186,3 +1186,109 @@ class Solution{
             return res;
         }
 };
+
+
+
+
+//99. Recover Binary Search Tree
+Solved
+Medium
+Topics
+Companies
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+Example 2:
+
+
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 1000].
+-231 <= Node.val <= 231 - 1
+ 
+
+Follow up: A solution using O(n) space is pretty straight-forward. Could you devise a constant O(1) space solution?
+
+
+
+// brute force
+// t:O(n) sp:O(n)
+class Solution{
+    private:
+        void f(TreeNode* root,vector<TreeNode*> &v){
+            if(root==NULL) return;
+            f(root->left,v);
+            v.push_back(root);
+            f(root->right,v);
+        }
+    public:
+        void recoverTree(TreeNode* root) {
+            vector<TreeNode*> v;
+            f(root,v);
+            TreeNode* first=NULL;
+            TreeNode* second=NULL;
+            TreeNode* temp=NULL;
+            for(int i=1;i<v.size();i++){
+                if(v[i]->val<v[i-1]->val){
+                    if(first==NULL){
+                        first=v[i-1];
+                        temp=v[i];
+                    }
+                    else{
+                        second=v[i];
+                    }
+                }
+            }
+            if(second==NULL) swap(first->val,temp->val);
+            else{
+                swap(first->val,second->val);
+            }
+        }
+};
+
+
+
+// optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    private:
+        void f(TreeNode* root){
+            if(root==NULL) return;
+            f(root->left);
+            if(prev && root->val<prev->val){
+                if(first==NULL){
+                    first=prev;
+                    temp=root;
+                }
+                else{
+                    second=root;
+                }
+            }
+            prev=root;
+            f(root->right);
+        }
+    public:
+        TreeNode* prev=NULL;
+        TreeNode* first=NULL;
+        TreeNode* second=NULL;
+        TreeNode* temp=NULL;
+        void recoverTree(TreeNode* root) {
+            f(root);
+            if(!second) swap(first->val,temp->val);
+            else{
+                swap(first->val,second->val);
+            }
+        }
+};
