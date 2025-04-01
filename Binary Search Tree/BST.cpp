12a605134be34543851046229709315f93a98246
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-01 18:59:24 +0530

    Largest BST

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -1093,3 +1093,96 @@ class Solution {
             return false;
         }
 };
+
+
+
+//Largest BST
+Difficulty: MediumAccuracy: 29.73%Submissions: 167K+Points: 4Average Time: 40m
+Youre given a binary tree. Your task is to find the size of the largest subtree within this binary tree that also satisfies the properties of a Binary Search Tree (BST). The size of a subtree is defined as the number of nodes it contains.
+
+Note: A subtree of the binary tree is considered a BST if for every node in that subtree, the left child is less than the node, and the right child is greater than the node, without any duplicate values in the subtree.
+
+Examples :
+
+Input: root = [5, 2, 4, 1, 3]
+Root-to-leaf-path-sum-equal-to-a-given-number-copy
+Output: 3
+Explanation:The following sub-tree is a BST of size 3
+Balance-a-Binary-Search-Tree-3-copy
+Input: root = [6, 7, 3, N, 2, 2, 4]
+
+Output: 3
+Explanation: The following sub-tree is a BST of size 3:
+
+Constraints:
+1 ≤ number of nodes ≤ 105
+1 ≤ node->data ≤ 105
+
+
+
+// brute force solution
+// t:O(n*n)  sp:O(n)
+class Solution{
+    private:
+        int size(Node* root){
+            if(root==NULL) return 0;
+            return size(root->left)+size(root->right)+1;
+        }
+        bool isBST(Node* root, int lb,int ub){
+            if(root==nullptr) return true;
+            bool left=isBST(root->left,lb,root->data);
+            bool right=isBST(root->right,root->data,ub);
+            if(left==false || right==false) return false;
+            if(root->data<=lb || root->data>=ub) return false;
+            return true;
+        }
+        void f(Node* root){
+            if(root==NULL) return ;
+            f(root->left);
+            if(isBST(root,INT_MIN,INT_MAX)) res=max(res,size(root));
+            f(root->right);
+        }
+    public:
+        int res=0;
+        int largestBst(Node *root){
+            f(root);
+            return res;
+        }
+};
+
+
+
+// optimal solution
+// t:O(n) sp:O(h)
+class Solution{
+    private:
+        int res=0;
+        vector<int> f(Node* root){
+            if(root->left==NULL && root->right==NULL){
+                res=max(res,1);
+                return {root->data,root->data,1};
+            }
+            int lmin=root->data,lmax=root->data,lsize=0;
+            int rmin=root->data,rmax=root->data,rsize=0;
+            if(root->left){
+                vector<int> vleft=f(root->left);
+                lmin=vleft[0];lmax=vleft[1];lsize=vleft[2];
+            }
+            if(root->right){
+                vector<int> vright=f(root->right);
+                rmin=vright[0];rmax=vright[1];rsize=vright[2];
+            }
+            if(rsize==-1|| lsize==-1) return {INT_MIN,INT_MAX,-1};
+            if(rsize!=-1 && lsize!=-1){
+                if(root->left && root->data<=lmax) return {INT_MIN,INT_MAX,-1};
+                if(root->right && root->data>=rmin) return {INT_MIN,INT_MAX,-1};
+            }
+            res=max(res,lsize+rsize+1);
+            return {lmin,rmax,lsize+rsize+1};
+        }   
+    public:
+        int largestBst(Node *root){
+            f(root);
+            return res;
+        }
+};
