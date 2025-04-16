1bb34da4dd40185e255bcddcdeb483deb9ffbc1d
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-17 01:49:59 +0530

    Shortest Job first

--- Greedy/Greddy.cpp
+++ Greedy/Greddy.cpp
@@ -733,3 +733,56 @@ class Solution {
             return count;
         }
 }; 
+
+
+
+//Shortest Job first
+Difficulty: MediumAccuracy: 68.79%Submissions: 29K+Points: 4Average Time: 30m
+Geek is a software engineer. He is assigned with the task of calculating average waiting time of all the processes by following shortest job first policy.
+
+The shortest job first (SJF) or shortest job next, is a scheduling policy that selects the waiting process with the smallest execution time to execute next.
+
+Given an array of integers bt of size n. Array bt denotes the burst time of each process. Calculate the average waiting time of all the processes and return the nearest integer which is smaller or equal to the output.
+
+Note: Consider all process are available at time 0.
+
+Example 1:
+
+Input:
+n = 5
+bt = [4,3,7,1,2]
+Output: 4
+Explanation: After sorting burst times by shortest job policy, calculated average waiting time is 4.
+Example 2:
+
+Input:
+n = 4
+arr = [1,2,3,4]
+Output: 2
+Explanation: After sorting burst times by shortest job policy, calculated average waiting time is 2.
+Your Task:
+You dont need to read input or print anything. Your task is to complete the function solve() which takes bt[] as input parameter and returns the average waiting time of all the processes.
+
+Expected Time Complexity: O(nlog(n))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n <= 105
+1 <= arr[i] <= 105
+
+//solution
+
+class Solution {
+    public:
+      long long solve(vector<int>& arr) {
+          int n=arr.size();
+          long long sum=0;
+          sort(arr.begin(),arr.end());
+          long long prev=0;
+          for(int i=0;i<n;i++){
+              sum= (long long)(sum+prev);
+              prev= (long long)(prev+arr[i]);
+          }
+          return sum/n;
+      }
+  };
