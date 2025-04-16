62163c5f924d195c285ad5c4783c4999cfd9fd4a
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-17 01:26:09 +0530

    Non-overlapping Intervals

--- Greedy/Greddy.cpp
+++ Greedy/Greddy.cpp
@@ -672,3 +672,64 @@ class Solution {
             return ans;
         }
 };
+
+
+//435. Non-overlapping Intervals
+Solved
+Medium
+Topics
+Companies
+Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 105
+intervals[i].length == 2
+-5 * 104 <= starti < endi <= 5 * 104
+
+
+// solution
+// t:O(nlogn) sp:O(1)
+class Solution {
+    private:
+        static bool cmp(vector<int> &a, vector<int> &b){
+            return a[1]<b[1];
+        }
+    public:
+        int eraseOverlapIntervals(vector<vector<int>>& arr) {
+            int n=arr.size();
+            int count=0;
+            sort(arr.begin(),arr.end(),cmp);
+            int end=arr[0][1];
+            for(int i=1;i<n;i++){
+                if(arr[i][0] < end){
+                    count++;
+                }
+                else{
+                    end=arr[i][1];
+                }
+            }
+            return count;
+        }
+}; 
