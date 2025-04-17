67420e3fb69b498766542cd96335ccb4eb720143
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-17 19:03:29 +0530

    Jump Game II

--- Greedy/Greddy.cpp
+++ Greedy/Greddy.cpp
@@ -785,4 +785,113 @@ class Solution {
           }
           return sum/n;
       }
-  };
+};
+
+
+
+//45. Jump Game II
+Solved
+Medium
+Topics
+Companies
+Amazon
+Apple
+Bloomberg
+Google
+DoorDash
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+Its guaranteed that you can reach nums[n - 1].
+
+
+
+// brute force
+// t:O(n*n) sp:O(n)
+class Solution {
+private:
+    int f(int ind,vector<int> &arr){
+        if(ind>=arr.size()-1) return 0;
+        int jump=1e8;
+        for(int i=1;i<=arr[ind];i++){
+            jump=min(jump,f(ind+i,arr));
+        }
+        return jump+1;
+    }
+public:
+    // brute force
+    int jump(vector<int>& arr) {
+       return f(0,arr);
+    }
+};
+
+// you can further optimize it using dp to O(n*n) time complexity
+
+// better solution
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int jump(vector<int>& arr) {
+            // brute force
+            int n=arr.size();
+            int count=0;
+            int i=0;
+            while(i<n){
+                if(i==n-1) return count;
+                if(i+arr[i]>=n-1) return count+1;
+                int maxe=-1,ind=i;
+                for(int j=1;j<=arr[i];j++){
+                    int val=i+j;
+                    if(val+arr[val]>=maxe){
+                        maxe=val+arr[val];ind=val;
+                    }
+                }
+                i=ind;count++;
+            }
+            return count;
+        }
+};
+
+
+
+// optimal solution
+class Solution {
+    public:
+        int jump(vector<int>& arr) {
+            int n=arr.size();
+            int maxIndex=arr[0];
+            int maxe=arr[0];
+            int count=0;
+            for(int i=1;i<n;i++){
+                if(maxIndex>=n-1) return count+1;
+                maxe=max(maxe,i+arr[i]);
+                if(i==maxIndex){
+                    count++;
+                    maxIndex=maxe;
+                }
+            }
+            return count;
+        }
+};
