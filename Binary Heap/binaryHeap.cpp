bb023efc3d393c970b513642cff6f3d67bcdd802
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-20 19:42:06 +0530

    Nearly sorted

--- Binary Heap/binaryHeap.cpp
+++ Binary Heap/binaryHeap.cpp
@@ -409,3 +409,66 @@ class Solution {
           return -1;
       }
 };
+
+
+//Nearly sorted
+Difficulty: MediumAccuracy: 75.25%Submissions: 70K+Points: 4Average Time: 30m
+Given an array arr[], where each element is at most k away from its target position, you need to sort the array optimally.
+Note: You need to change the given array arr[] in place.
+
+Examples:
+
+Input: arr[] = [6, 5, 3, 2, 8, 10, 9], k = 3
+Output: [2, 3, 5, 6, 8, 9, 10]
+Explanation: The sorted array will be 2 3 5 6 8 9 10
+Input: arr[]= [1, 4, 5, 2, 3, 6, 7, 8, 9, 10], k = 2
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Explanation: The sorted array will be 1 2 3 4 5 6 7 8 9 10
+Dont use the inbuilt sort() function for this question.
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ k < arr.size()
+1 ≤ arri ≤ 106
+
+
+
+// brute force
+// t:O(n^2logn) sp:O(1)
+
+class Solution {
+    public:
+      void nearlySorted(vector<int>& arr, int k) {
+          int n=arr.size();
+          int low=0,high=k;
+          while(high<n){
+              sort(arr.begin()+low,arr.begin()+high+1);
+              low++;high++;
+          }
+      }
+};
+
+// optimal solution
+
+class Solution {
+    public:
+      void nearlySorted(vector<int>& arr, int k) {
+          int n=arr.size();
+          priority_queue<int,vector<int>,greater<int>> pq;
+          int i,ind=0;
+          for(i=0;i<=k;i++){
+              pq.push(arr[i]);
+          }
+          while(i<n){
+              arr[ind]=pq.top();
+              pq.pop();
+              pq.push(arr[i]);
+              i++;ind++;
+          }
+          while(ind<n){
+              arr[ind]=pq.top();
+              pq.pop();
+              ind++;
+          }
+      }
+};
