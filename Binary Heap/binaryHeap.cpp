a6ab357da1f10aa11e7a51367600d57002a289c7
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-04-23 00:11:45 +0530

    Task Scheduler

--- Binary Heap/binaryHeap.cpp
+++ Binary Heap/binaryHeap.cpp
@@ -705,3 +705,162 @@ class Solution{
         }
     
 };
+
+
+
+
+//621. Task Scheduler
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an array of CPU tasks, each labeled with a letter from A to Z, and a number n. Each CPU interval can be idle or allow the completion of one task. Tasks can be completed in any order, but there's a constraint: there has to be a gap of at least n intervals between two tasks with the same label.
+
+Return the minimum number of CPU intervals required to complete all tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+
+Output: 8
+
+Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
+
+After completing task A, you must wait two intervals before doing A again. The same applies to task B. In the 3rd interval, neither A nor B can be done, so you idle. By the 4th interval, you can do A again as 2 intervals have passed.
+
+Example 2:
+
+Input: tasks = ["A","C","A","B","D","B"], n = 1
+
+Output: 6
+
+Explanation: A possible sequence is: A -> B -> C -> D -> A -> B.
+
+With a cooling interval of 1, you can repeat a task after just one other task.
+
+Example 3:
+
+Input: tasks = ["A","A","A", "B","B","B"], n = 3
+
+Output: 10
+
+Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+
+There are only two types of tasks, A and B, which need to be separated by 3 intervals. This leads to idling twice between repetitions of these tasks.
+
+ 
+
+Constraints:
+
+1 <= tasks.length <= 104
+tasks[i] is an uppercase English letter.
+0 <= n <= 100
+
+// solution1 using heap(priority_queue)
+// t:O(n) sp:O(1) preffered solution of my version
+class Solution {
+    public:
+        int leastInterval(vector<char>& arr, int k) {
+            int n=arr.size();
+            vector<int> v(26,0);
+            for(int i=0;i<n;i++){
+                v[arr[i]-'A']++;
+            }
+            priority_queue<int> pq;
+            for(int i=0;i<26;i++){
+                if(v[i]>0){
+                    pq.push(v[i]);
+                }
+            }
+            int res=0;
+            while(!pq.empty()){
+                vector<int> temp;
+                for(int i=0;i<=k;i++){
+                    if(!pq.empty()){
+                        int freq=pq.top();
+                        pq.pop();
+                        temp.push_back(freq-1);
+                    }
+                }
+                for(int i=0;i<temp.size();i++){
+                    if(temp[i]>0){
+                        pq.push(temp[i]);
+                    }
+                }
+                res+=(pq.empty())?temp.size():k+1;
+            }
+            return res;
+        }
+};
+
+// my code
+class Solution {
+    public:
+        int leastInterval(vector<char>& arr, int k) {
+            int n=arr.size();
+            vector<int> v(26,0);
+            for(int i=0;i<n;i++){
+                v[arr[i]-'A']++;
+            }
+            priority_queue<pair<int,int>,vector<pair<int,int>>> pq;
+            for(int i=0;i<26;i++){
+                if(v[i]>0){
+                    pq.push({v[i],i});
+                    v[i]=0;
+                }
+            }
+            for(int i=0;i<26;i++){
+                v[i]=0;
+            }
+            int idle_count=0;
+            while(!pq.empty()){
+                int count=k+1;
+                while(count>0 && !pq.empty()){
+                    auto [freq,ind] = pq.top();
+                    pq.pop();
+                    v[ind]=freq-1;
+                    count--;
+                }
+                int sum=accumulate(v.begin(),v.end(),0);
+                if(sum){
+                    while(count>0){
+                        idle_count++;
+                        count--;
+                    }
+                    for(int i=0;i<26;i++){
+                        if(v[i]>0){
+                            pq.push({v[i],i});
+                            v[i]=0;
+                        }
+                    }
+                }
+            }
+            return n+idle_count;
+        }
+};
+
+
+
+// optimal solution
+// t:O(n) sp:O(1)
+class Solution {
+    public:
+        int leastInterval(vector<char>& arr, int n) {
+            int size= arr.size();
+            vector<int> freq(26,0);
+            for(int i=0;i<size;i++){
+                freq[arr[i]-'A']++;
+            }
+            sort(freq.begin(),freq.end());
+            int maxFreq= freq[25]-1;
+            int idle_slots= n*maxFreq;
+            for(int i=24;i>=0;i--){
+                idle_slots-=min(maxFreq,freq[i]);
+            }
+            return (idle_slots>0)? size+idle_slots: size;
+        }
+};
+
